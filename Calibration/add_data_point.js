ee67199499e7c4971ecccc2353182cedd7ac757f
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2007-09-24 20:34:57 +0000

    Fixed up formating and added some comments.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@513 6e01202a-0783-4428-890a-84243c50cc2b

--- Calibration/add_data_point.js
+++ Calibration/add_data_point.js
@@ -2,7 +2,7 @@ function clicked() {
 	java.lang.System.out.println("clicked");
 
 	numSamples = sourceDataStore.getTotalNumSamples();
-          if(numSamples <= 0){
+    if(numSamples <= 0){
 		java.lang.System.out.println("can't add point with no samples");
 		return;
 	}
@@ -10,7 +10,11 @@ function clicked() {
 	value = sourceDataStore.getValueAt(numSamples-1, 1);
 
 	numCalibrationPoints = calibrationPoints.getTotalNumSamples();
+	
+	// Add the uncalibrated value
 	calibrationPoints.setValueAt(numCalibrationPoints, 0, new java.lang.Float(value));	
+	
+	// Add a default value (0) for the calibrated value
 	calibrationPoints.setValueAt(numCalibrationPoints, 1, new java.lang.Float(0));	
 	
 }
