29956b2f34ec76b44f76840bfae91cb22dee545f
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2007-10-03 22:34:34 +0000

    A file for testing exceptions in scripts.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@593 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ Script/exception-test.js
@@ -0,0 +1,13 @@
+importClass(Packages.java.lang.System);
+importClass(Packages.java.lang.Class);
+
+
+function init() {
+	// this is to test the exception handling.
+	var nothing;
+	return true;
+}
+
+function save()
+{
+}
