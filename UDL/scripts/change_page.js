8910003f6a3c403a088410b1a95cf99c8b6efc64
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2007-11-07 19:27:32 +0000

    script was using old question class
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@8703 6e01202a-0783-4428-890a-84243c50cc2b

--- UDL/scripts/change_page.js
+++ UDL/scripts/change_page.js
@@ -17,7 +17,6 @@
 
 importClass(Packages.java.awt.event.ActionListener);
 importClass(Packages.java.util.Vector);
-importClass(Packages.org.concord.otrunk.udl3.OTQuestionList);
 importClass(Packages.org.concord.framework.otrunk.OTObjectList);
 importClass(Packages.javax.swing.JOptionPane);
 
