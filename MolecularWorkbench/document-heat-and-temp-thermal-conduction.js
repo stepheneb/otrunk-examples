c67adfa403fc8f2a7a45c71ba95f7c2bd01ba753
aunger <aunger@6e01202a-0783-4428-890a-84243c50cc2b>
2007-12-04 21:37:20 +0000

    Revert the rest back to the originals
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@8957 6e01202a-0783-4428-890a-84243c50cc2b

--- MolecularWorkbench/document-heat-and-temp-thermal-conduction.js
+++ MolecularWorkbench/document-heat-and-temp-thermal-conduction.js
@@ -11,8 +11,6 @@ importPackage(Packages.org.concord.framework.data.stream);
 var runButton;
 var stopButton;
 var resetButton;
-var timeSlider;
-var stopTime;
 
 ////
 //
@@ -20,49 +18,82 @@ var stopTime;
 //
 ////
 var timer;
-
-var ck_values = datastore_ck.getValues();
-var ws_values = datastore_ws.getValues();
-var pl_values = datastore_pl.getValues();
-var nt_values = datastore_nt.getValues();
-
-var temp_ck = 0;
-var temp_ws = 0;
-var temp_pl = 0;
-var temp_nt = 0;
-
-var temp_ck_scaler = 1;
-var temp_ws_scaler = 1;
-var temp_pl_scaler = 1;
-var temp_nt_scaler = 1;
-
-var a = 0.02; // what part of the current actual temperature to use in the smoothed temp
-var f = 10; // K.E. scale factor
-var n = (1.6/1.38 * 10000.00); // ~ number of deg K in 1 eV
-var b = 0; // increment degC by this amount
+var heatChange = 0.5;
+var dt = 0.5; 
+var dataProducer,dataProducer;
+var d1Values = datastore1.getValues();
+var d2Values = datastore2.getValues();
+var temp =0,temp2=0;
+var a = 0.02;
+var f = 2; // K.E. scale factor
+var n = (1.6/1.38 * 10000.00);
+var b = 20;
 var m1 = 1;
 var m2 = 0.002;
 var constant = 0;   // 273
 var timeCounter = 0;
-var xMax = 60;
-var yMax = 50;
-var yMin = -2;
+var viewContainer;
+var xMax = 56;
+var yMax = 100;
+var yMin = 0;
 var xMin = -2;
-
-var stepTime = 500;
-var counterIncrement = stepTime/1000;
 
 var page = modelComponent.getComponent(0);
 var model;
 
-var pageListener = new PageListener() {
-	pageUpdate: function(event) {
-		if (event.getType() == PageEvent.PAGE_READ_END) {
-			postMWInit();
+var mouseHandler =
+{
+	mouseClicked:function(evt)
+	{
+		// System.err.println("Mouse clicked. Src: " + evt.getSource());
+		if (evt.getSource().equals(viewContainer))
+		{
+			// System.err.println("viewContainer clicked");
+			if (evt.getSource().getAction() == Packages.org.concord.mw2d.UserAction.HEAT_ID)
+			{
+				temp = (1*m2)*(n*f*model.getKinForType(Element.ID_CK)-constant)+b*1
+				temp2 = (1*m2)*(n*f*model.getKinForType(Element.ID_WS)-constant)+b*1
+				graphHeater(temp,temp2);
+			}
+		}
+
+	}
+}
+var mouseListener = new MouseListener(mouseHandler);
+
+var runButtonHandler =
+{
+	actionPerformed :function(evt)
+	{
+		// System.err.println("Start action recieved");
+		if (! timer.isRunning()) {
+			if (timeCounter == 0)
+			{
+					temp = (1*m2)*(n*f*model.getKinForType(Element.ID_CK)-constant)+b*1;
+					temp2 =(1*m2)*(n*f*model.getKinForType(Element.ID_WS)-constant)+b*1;
+					graphHeater(temp, temp2);
+			}
+			
+			// System.err.println("Starting timer");
+			timer.start();
 		}
 	}
 }
-page.addPageListener(pageListener);
+var runButtonListener = new ActionListener(runButtonHandler);
+
+var stopButtonHandler =
+{
+	actionPerformed :function(evt)
+	{
+		// System.err.println("Stop action recieved");
+			if (timer.isRunning())
+			{
+				// System.err.println("Stopping timer");
+				timer.stop();
+			}
+	}
+}
+var stopButtonListener = new ActionListener(stopButtonHandler);
 
 var resetButtonHandler =
 {
@@ -79,151 +110,87 @@ var resetButtonHandler =
 }
 var resetButtonListener = new ActionListener(resetButtonHandler);
 
-var modelListener = new ModelListener() {
-	modelUpdate: function(event) {
-		if (event.getID() == ModelEvent.MODEL_RESET) {
-			// System.err.println("Reset action recieved");
-			if (timer.isRunning())
-			{
-				// System.err.println("Stopping timer");
-				timer.stop();
-			}
-			resetGraph();
-		} else if (event.getID() == ModelEvent.MODEL_RUN) {
-			// System.err.println("Start action recieved");
-			if (! timer.isRunning()) {
-				stopTime = timeSlider.getValue()*60;
-				// System.err.println("Stop time is: " + stopTime);
-				if (timeCounter == 0) {
-					  // record the temperature now -- this is what the sliders are set to
-						temp_ck = getCurrentTempForType(Element.ID_CK);
-						temp_ws = getCurrentTempForType(Element.ID_WS);
-						temp_pl = getCurrentTempForType(Element.ID_PL);
-						temp_nt = getCurrentTempForType(Element.ID_NT);
-						// graphHeater(temp_ck, temp_ws, temp_pl, temp_nt);
-				}
-				
-				// System.err.println("Starting timer");
-				timer.start();
-			}
-		} else if (event.getID() == ModelEvent.MODEL_STOP) {
-			// System.err.println("Stop action recieved");
-			if (timer.isRunning())
-			{
-				// System.err.println("Stopping timer");
-				timer.stop();
-			}
+var pageListener = new PageListener() {
+	pageUpdate: function(event) {
+		if (event.getType() == PageEvent.PAGE_READ_END) {
+			postMWInit();
 		}
 	}
 }
+page.addPageListener(pageListener);
 
 var timerHandler =
 { 
 	actionPerformed:function(evt)
 	{
-		// System.err.println("timer run: " + timeCounter);
-		if (! model.isRunning()) {
-			timer.stop();
-		}
-		
-		if (timeCounter == 0) {
-			// for some reason the temp recorded here is always substantially lower than the temp the sliders are set to
-			// so using our first recording, calculate a scaling factor
-			temp_ck_scaler = (temp_ck/getCurrentTempForType(Element.ID_CK));
-			temp_ws_scaler = (temp_ws/getCurrentTempForType(Element.ID_WS));
-			temp_pl_scaler = (temp_pl/getCurrentTempForType(Element.ID_PL));
-			temp_nt_scaler = (temp_nt/getCurrentTempForType(Element.ID_NT));
-			
-			if (temp_ck_scaler <= 0)
-			  temp_ck_scaler = 1;
-			if (temp_pl_scaler <= 0)
-			  temp_pl_scaler = 1;
-			if (temp_nt_scaler <= 0)
-			  temp_nt_scaler = 1;
-			if (temp_ws_scaler <= 0)
-			  temp_ws_scaler = 1;
-		
-			temp_ck = getCurrentTempForType(Element.ID_CK) * temp_ck_scaler;
-			temp_ws = getCurrentTempForType(Element.ID_WS) * temp_ws_scaler;
-			temp_pl = getCurrentTempForType(Element.ID_PL) * temp_pl_scaler;
-			temp_nt = getCurrentTempForType(Element.ID_NT) * temp_nt_scaler;
-			graphHeater(temp_ck, temp_ws, temp_pl, temp_nt);
-		} else {
-			// calculate the exponential moving average
-			temp_ck = a*(getCurrentTempForType(Element.ID_CK)*temp_ck_scaler)+(1-a)*temp_ck;
-			temp_ws = a*(getCurrentTempForType(Element.ID_WS)*temp_ws_scaler)+(1-a)*temp_ws;
-			temp_pl = a*(getCurrentTempForType(Element.ID_PL)*temp_pl_scaler)+(1-a)*temp_pl;
-			temp_nt = a*(getCurrentTempForType(Element.ID_NT)*temp_nt_scaler)+(1-a)*temp_nt;
-			graphValues(temp_ck, temp_ws, temp_pl, temp_nt);
-		}
-		
+		// System.err.println("timer run");
+		temp = a*((1*m2)*(n*f*model.getKinForType(Element.ID_CK)-constant)+1*b)+(1-a)*temp;
+		temp2 = a*((1*m2)*(n*f*model.getKinForType(Element.ID_WS)-constant)+1*b)+(1-a)*temp2;
+		graphValues(temp, temp2);
+
 		// graph.repaint()
-		timeCounter += counterIncrement;
-		if (timeCounter > stopTime) {
-			timer.stop();
+		timeCounter +=0.5
+		//if (timeCounter>30)
+		if ((Math.round(temp) == Math.round(temp2))) {
+		 if (Math.round(temp) !=1*b)
+		{
 			model.stop();
+			timer.stop();
+			//currentNode.next();
 		}
-		// if (timeCounter>30) {
-			if (  (Math.round(temp_ck) <= Math.round(temp_ws)) ||
-				  (Math.round(temp_ck) <= Math.round(temp_pl)) ||
-				  (Math.round(temp_ck) <= Math.round(temp_nt)) ) {
-					model.stop();
-					timer.stop();
-			}
-		// }
+		}
 	}
 }
 var timerListener = new ActionListener(timerHandler);
 
 function init() {
-	timer = new Timer(stepTime, timerListener);
-
+	timer = new Timer(500, timerListener);
+	// modelComponent.addMouseListener(mouseListener);
+	// page.addMouseListener(mouseListener);
 	return true;
 }
 
 function postMWInit() {
-	
-	var pageComps = page.getEmbeddedComponent(Class.forName("org.concord.modeler.PageButton")).values().toArray();
-	if (pageComps != null) {
-		for (var i = 0; i < pageComps.length; i++) {
-			var obj = pageComps[i];
-			if (obj.getText().equals("Reset")) {
-				resetButton = obj;
-			}
-		}
-	}
-	resetButton.addActionListener(resetButtonListener);
-	
-	var sliderComps = page.getEmbeddedComponent(Class.forName("org.concord.modeler.PageSlider")).values().toArray();
-	if (sliderComps != null) {
-		for (var i = 0; i < sliderComps.length; i++) {
-			var obj = sliderComps[i];
-			// System.err.println(obj.getTitle());
-			if (obj.getTitle().equals("Observation Time (Minute)")) {
-				timeSlider = obj;
-			}
-		}
-	}
-	
-	var models = page.getEmbeddedComponent(Class.forName("org.concord.modeler.ModelCanvas")).values().toArray();
-	if (models != null) {
-		for (var i = 0; i < models.length; i++) {
-			model = models[i].getContainer().getModel();
-			model.addModelListener(modelListener);
-		}
-	}
-	
+	// System.err.println("Model comp is: " + model.getComponent(0));
+				var pageComps = page.getEmbeddedComponent(Class.forName("org.concord.modeler.PageButton")).values().toArray();
+				// System.err.println("Page comps are (" + pageComps.length +"): " + pageComps);
+				if (pageComps != null) {
+					for (var i = 0; i < pageComps.length; i++) {
+						var obj = pageComps[i];
+						// System.err.println(obj.getText());
+						if (obj.getText().equals("Run")) {
+							runButton = obj;
+						}
+						else if (obj.getText().equals("Stop")) {
+							stopButton = obj;
+						}
+						else if (obj.getText().equals("Reset")) {
+							resetButton = obj;
+						}
+					}
+				}
+				runButton.addActionListener(runButtonListener);
+				stopButton.addActionListener(stopButtonListener);
+				resetButton.addActionListener(resetButtonListener);
+				
+				var models = page.getEmbeddedComponent(Class.forName("org.concord.modeler.ModelCanvas")).values().toArray();
+				if (models != null) {
+					for (var i = 0; i < models.length; i++) {
+						model = models[i].getContainer().getModel();
+						// System.err.println("The model is a: " + model.getClass().getName());
+						viewContainer = models[i].getContainer().getView();
+						viewContainer.addMouseListener(mouseListener);
+					}
+				}
+				
+				viewContainer.getPointHeater().setAmount(heatChange);
+				
 	initialGraph();
 	resetGraph();
 }
 
 function save() {
 	return true;
-}
-
-function getCurrentTempForType(type) {
-	// System.err.println(System.currentTimeMillis() + "," + type + "," + ((1*m2)*(n*f*model.getKinForType(type)-constant)+b*1));
-	return ((1*m2)*(n*f*model.getKinForType(type)-constant)+b*1);
 }
 
 function initialGraph()
@@ -239,60 +206,36 @@ function initialGraph()
 function resetGraph() {
 	
 	graph.reset();
-	
-	temp_ck = getCurrentTempForType(Element.ID_CK);
-	temp_ws = getCurrentTempForType(Element.ID_WS);
-	temp_pl = getCurrentTempForType(Element.ID_PL);
-	temp_nt = getCurrentTempForType(Element.ID_NT);
-	graphHeater(temp_ck, temp_ws, temp_pl, temp_nt);
-	
+	graphHeater((1*m2)*(n*f*model.getKinForType(Element.ID_CK)-constant)+b*1,
+					(1*m2)*(n*f*model.getKinForType(Element.ID_WS)-constant)+b*1);
 	timeCounter = 0;
 	yMax = 100;
 	xMax = 56;
 	graph.setLimitsAxisWorld(xMin,xMax,yMin,yMax);
 }
 
-function graphValues(t_ck, t_ws, t_pl, t_nt)
+function graphValues(t1, t2)
 {
-	// System.err.println(timeCounter + " -- ck: " + t_ck + ", ws: " + t_ws + ", pl: " + t_pl + ", nt: " + t_nt);
-	if (t_ck != b) {
-		ck_values.add(new Float(timeCounter));
-		ck_values.add(new Float(t_ck));
-	}
-
-	if (t_ws != b) {
-		ws_values.add(new Float(timeCounter));
-		ws_values.add(new Float(t_ws));
-	}
-	
-	if (t_pl != b) {
-		pl_values.add(new Float(timeCounter));
-		pl_values.add(new Float(t_pl));
-	}
-	
-	if (t_nt != b) {
-		nt_values.add(new Float(timeCounter));
-		nt_values.add(new Float(t_nt));
-	}
-	
-	resizeGraph(t_ck, t_ws, t_pl, t_nt);
+	// System.err.println("adding v1: " + timeCounter + ", " + t1);
+	d1Values.add(new Float(timeCounter));
+	d1Values.add(new Float(t1));
+	// System.err.println("adding v2: " + timeCounter + ", " + t2);
+	d2Values.add(new Float(timeCounter));
+	d2Values.add(new Float(t2));
+	resizeGraph(t1, t2);
 	graph.repaint();
 }
 
-function resizeGraph(t1, t2, t3, t4) {
-	var max = maximum(t1, t2);
-	// System.err.println("==================================");
-	// System.err.println("v1: " + t1 + ", v2: " + t2 + ", max: " + max);
-	// System.err.print("pmax: " + max);
-	max = maximum(max, t3);
-	// System.err.println(", v2: " + t3 + ", max: " + max);
-	// System.err.print("omax: " + max);
-	max = maximum(max, t4);
-	// System.err.println(", v2: " + t4 + ", max: " + max);
-	
-	if (max > (yMax-5)) {
+function resizeGraph(t1,t2) {
+	if (t1 > t2) {
+		if (t1 > (yMax-5)) {
+			// System.err.println("resizing y axis: " + (t1+5));
+			yMax = (t1+5);
+		}
+	}
+	else if (t2 > (yMax-5)) {
 		// System.err.println("resizing y axis: " + (t2+5));
-		yMax = (max+5);
+		yMax = (t2+5);
 	}
 	
 	if (timeCounter > (xMax - 5)) {
@@ -302,17 +245,9 @@ function resizeGraph(t1, t2, t3, t4) {
 	graph.setLimitsAxisWorld(xMin,xMax,yMin,yMax);
 }
 
-function maximum(v1, v2) {
-	if (v2 > v1) {
-		return v2;
-	}
-	
-	return v1;
-}
-
-function graphHeater(t_ck,t_ws,t_pl,t_nt)
+function graphHeater(t1,t2)
 {
 	graph.reset();
-	graphValues(t_ck, t_ws, t_pl, t_nt);
+	graphValues(t1, t2);
 	graph.repaint();
 }
