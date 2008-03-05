1538fa7dd5994eac6dc177a17121f0ed984e4c60
skim <skim@6e01202a-0783-4428-890a-84243c50cc2b>
2008-03-05 20:39:16 +0000

    Separated out the assess() function from capa_labview_activity.js and put in in a new file, capa_labview_assess.js.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@12780 6e01202a-0783-4428-890a-84243c50cc2b

--- CAPA/capa_labview_activity.js
+++ CAPA/capa_labview_activity.js
@@ -192,61 +192,6 @@ function validateAnswers() {
 	return true	
 }
 
-/**
- * Create rubric from the OTModelActivityData object by populating the OTAssessment object
- * This must happen AFTER the LabVIEW has exited, and AFTER validateAnswers().
- */
-function assess() {
-	var converter = new LabviewReportConverter(_monitor)
-	converter.markEndTime()
-	var madwrapper = converter.getMADWrapper()
-	_assessUtil = new ScopeAssessmentUtil(madwrapper)
-	
-	_correctAmp = 2 * Double.parseDouble(madwrapper.getLastCIValue("amplitude")) //peak-to-peak amplitude
-	_correctFrq = Double.parseDouble(madwrapper.getLastCIValue("frequency"))
-	
-	var ampUnit = _submittedAmpUnit.getAbbreviation()
-	var frqUnit = _submittedFrqUnit.getAbbreviation()
-	
-	var etime = _dateFormat.format(new Date())
-	log(etime + " - Correct amplitude = " + ScopeAssessmentUtil.getAmplitudeString(_correctAmp))
-	log(etime + " - Correct frequency = " + ScopeAssessmentUtil.getFrequencyString(_correctFrq) + " (T = " + ScopeAssessmentUtil.getPeriodString(1/_correctFrq) + ")")
-	
-	var numChanges = madwrapper.getNumChanges()
-	
-    log(etime + " - Answer submitted: amplitude = " + _submittedAmp + " " + ampUnit)
-  	log(etime + " - Answer submitted: frequency = " + _submittedFrq + " " + frqUnit)
-    
-	var ampIndicator = checkAmplitude(_correctAmp, _submittedAmp, ampUnit)
-	var frqIndicator = checkFrequency(_correctFrq, _submittedFrq, frqUnit)
-	var ampUnitIndicator = checkAmpUnit(ampUnit)
-	var frqUnitIndicator = checkFrqUnit(frqUnit)
-	var timeTotal = madwrapper.getTimeTotal()
-	var settingsIndicator = checkSettings(madwrapper)
-	
-	_otAssessment.setLabel("Oscilloscope")
-	var indicators = _otAssessment.getIndicatorValues()
-	indicators.put("amplitudeValue", ampIndicator)
-	indicators.put("frequencyValue", frqIndicator)
-	indicators.put("amplitudeUnit", ampUnitIndicator)
-	indicators.put("frequencyUnit", frqUnitIndicator)
-	indicators.put("numChanges", numChanges)
-	indicators.put("timeTotal", timeTotal)
-	indicators.put("controlSetting", settingsIndicator)
-	
-	++_currentStep
-	
-	if (_currentStep <= _lastStep){
-		startStep(_currentStep)
-	}
-	else {
-		endActivity()
-	}
-
-	log("----------")
-	_info.setText(_info.getText() + _assessUtil.getChangeLog())
-}
-
 function getDiff(a, b) {
 	return Math.abs(a - b) / b	
 }
