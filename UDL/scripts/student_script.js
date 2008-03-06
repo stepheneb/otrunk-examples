9739407149ca9e82d6c2bac5aff28cbe548a6cd8
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2008-03-06 20:51:35 +0000

    overlay author script with a student script which does nothing (for now)} this will a) stop the students from creating delta objects as the script changes each page, and b) speed things up for the student
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@12801 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ UDL/scripts/student_script.js
@@ -0,0 +1,18 @@
+/*
+ * This is a set of scripts which help in the authoring of new UDL activities.
+ * The script fills in new pages and section with default content and inserts
+ * new menus in all new sections.
+ * The script is designed to be attached to the "script" element of the UDL container,
+ * which allows it to be always running.
+ *
+ * @author sfentress
+ */
+  
+
+function init() {
+	return true;
+}
+
+function save() {
+	return true;
+}
