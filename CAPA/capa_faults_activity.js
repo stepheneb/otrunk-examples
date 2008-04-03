f7b6921ecf6f21ea1bebb476a9d520f941ca036b
skim <skim@6e01202a-0783-4428-890a-84243c50cc2b>
2008-04-03 22:02:50 +0000

    Files for new LabVIEW activity (Locating faults in circuit)
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@13133 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ CAPA/capa_faults_activity.js
@@ -0,0 +1,127 @@
+importClass(Packages.java.lang.System)
+importClass(Packages.java.lang.Integer)
+importClass(Packages.java.lang.Float)
+importClass(Packages.java.lang.Double)
+importClass(Packages.java.text.SimpleDateFormat)
+importClass(Packages.java.awt.Color)
+importClass(Packages.java.awt.event.ActionListener)
+importClass(Packages.javax.swing.JOptionPane)
+importClass(Packages.java.math.RoundingMode)
+
+importClass(Packages.org.concord.otrunk.ui.OTText)
+importClass(Packages.org.concord.otrunk.ui.swing.OTCardContainerView)
+importClass(Packages.org.concord.otrunkcapa.rubric.OTAssessment)
+importClass(Packages.org.concord.otrunk.labview.LabviewMonitor)
+importClass(Packages.org.concord.otrunk.labview.LabviewReportConverter)
+
+/*
+ * Variables from OTScriptObject
+ * 
+ * otContents
+ * controllerService
+ */
+ 
+/*
+ * Variables from otml 
+ * otc_ : component variables
+ */
+ 
+var ot_monitor
+var ot_cards
+var otc_submitButton
+var otc_reportButton
+ 	
+var glob = {
+	dateFormat : SimpleDateFormat.getInstance(),
+	info : null, // text to be added to the report
+	currentStep : 1,
+	lastStep : 1,
+	monitor : null, // "real object" for ot_monitor
+	otAssessment : null,
+	activityLog : ""
+}
+
+ /**
+ * This function is called when the script starts up
+ * It returns a boolean indicating whether the initialization 
+ * was successful or not.
+ */
+function init() {
+	System.out.println("Entered: init()")
+	glob.dateFormat.applyPattern("MM/dd/yyyy HH:mm:ss zzz")	
+	setupGUI();
+    glob.monitor = controllerService.getRealObject(ot_monitor)		
+    setupAssessmentLogging()
+	initLogging()
+	OTCardContainerView.setCurrentCard(ot_cards, "main_card_1")	
+	return true
+}
+
+function save() {
+	System.out.println("Entered: save()")
+    otc_submitButton.removeActionListener(listeners.submitButtonListener)
+	return true
+}
+
+function setupAssessmentLogging() {
+	// Create assessment object
+	glob.otAssessment = otObjectService.createObject(OTAssessment)
+	glob.otAssessment.setTitle("Faults - Student Report")
+	otContents.add(glob.otAssessment)
+}
+
+function initLogging() {
+	glob.info = otObjectService.createObject(OTText)
+	glob.info.setText("")
+	otContents.add(glob.info)
+}
+
+function log(msg) {
+	glob.info.setText(glob.info.getText() + msg)
+}
+
+function setupGUI() {
+	otc_reportButton.setVisible(false)	
+	otc_submitButton.addActionListener(listeners.submitButtonListener)
+}
+
+function endActivity() {
+	otc_reportButton.setVisible(true)
+	OTCardContainerView.setCurrentCard(ot_cards, "main_card_2")
+}
+
+function wrap_assess() {
+	var converter = new LabviewReportConverter(glob.monitor)
+	converter.markEndTime()
+	var madWrapper = converter.getMADWrapper()
+	glob.otAssessment.setLabel("Fault")
+	var inventory = glob.otAssessment.getInventory()
+	inventory.put("modelActivityData", converter.getOTModelActivityData())
+	assess(glob.otAssessment, madWrapper)
+}
+
+var listeners = {
+	submitButtonListener : new ActionListener({
+		actionPerformed: function(evt) {
+			p("Enter: submitButtonListener.actionPerformed()")
+
+	    	if (glob.monitor.hasNeverRun()) {
+	    		var msg = "You must do the activity first before submission. Click on \"show circuit\".";
+	    		var option = JOptionPane.showMessageDialog(null, msg, "Alert", JOptionPane.WARNING_MESSAGE)
+	    		return
+	    	}
+			var msg = "This will end your activity and close the LabVIEW window. Do you want to continue?"
+			var option = JOptionPane.showConfirmDialog(null, msg, "Submitting Answer", JOptionPane.OK_CANCEL_OPTION)
+			if (option == JOptionPane.OK_OPTION) {
+	  			glob.monitor.close()
+	    		wrap_assess() // must close labVIEW before assess()
+				endActivity()
+			}
+		}
+	})
+}
+
+// for debugging
+function p(s) {
+	System.out.println(s)
+}
---
+++ CAPA/capa_faults_assess.js
@@ -0,0 +1,90 @@
+// Holder for answers
+var answers =  { 
+	s_fault : 0, // submitted fault location
+	c_fault : 0, // correct fault location
+	numChanges : 0,
+}
+
+// Maps used to interpret the data from the LabVIEW VI
+// fault: a number that identifies a fault
+// gate: a number that identifies a gate
+// For example, if fault id is 1, we can find by faultToGate[1] = 3 that gate 3 is faulty,
+// then by looking up gateToLabel[3] find that it means inverter U1C.
+var viMap = {
+	gateToLabel : [
+		"No faults", "Inverter U1A", "Inverter U1B", "Inverter U1C", "AND gate U2A", "AND gate U2B", "AND gate U3B", 
+		"OR gate U3A", "OR gate U3B", "OR gate U3C"		
+	],
+	faultToGate : [ 0, 3, 6, 4, 5, 8],
+	probeMap : [ "No Connection", "U1A Input", "U1A Output", "U1B Input", "U1B Output",
+		"U1C Input", "U1C Output", "U1 Ground", "U1 VCC", "U2A Input1",
+		"U2A Input 2", "U2A Input 3", "U2A Output", "U2B Input 1", "U2B Input 2",
+		"U2B Input 3", "U2B Output", "U2C Input 1", "U2C Input 2", "U2C Input 3",
+		"U2C Output", "U2 Ground", "U2 VCC", "U3A Input 1", "U3A Input 2",
+		"U3A Output", "U3B Input 1", "U3B Input 2", "U3B Output", "U3 Ground",
+		"U3 VCC"
+	],
+}
+
+function getCorrectFaultyGate(madWrapper) {
+	var faultID = parseInt(madWrapper.getLastCIValue("Fault"))
+	return viMap.faultToGate[faultID]
+}
+
+function getSubmittedFaultyGate(madWrapper) {
+	return parseInt(madWrapper.getLastCIValue("Fault List")) 
+}
+
+function processUserEvents(madWrapper) {
+	var civs = madWrapper.getSortedCIVs();
+	for (var i = 0; i < civs.size(); ++i) {
+		var civ = civs.get(i)
+		var name = civ.getName()
+					
+		if (name == "A" || name == "B" || name == "C" || 
+			name == "Probe Location")
+		{
+			++ answers.numChanges;
+			glob.activityLog += getLogLine(civ)	
+		}
+	}
+}
+
+function getLogLine(civ) {
+	var name = civ.getName()
+	var value = civ.getValue()
+	var time = glob.dateFormat.format(new Date(civ.getTime()))
+	
+	if (name == "A" || name == "B" || name == "C") {
+		return time + " - Input " + name + " flipped to " + value + "\n"
+	}
+	else if (name == "Probe Location") {
+		return time + " - Probed [" + viMap.probeMap[parseInt(value)] + "]\n"		
+	}		
+}
+
+/**
+ * This function mut be called AFTER the LabVIEW has exited, and AFTER validateAnswers().
+ */
+function assess(assessment, madWrapper) {
+	p("ENTER: assess()")
+
+	processUserEvents(madWrapper)
+	answers.c_fault = getCorrectFaultyGate(madWrapper) // correct answer
+	answers.s_fault = getSubmittedFaultyGate(madWrapper) // submitted answer
+	
+	var etime = glob.dateFormat.format(new Date())
+	log("----------\n")
+	log(etime + " - Correct fault location = " + viMap.gateToLabel[answers.c_fault] + "\n")
+  	log(etime + " - Submitted fault location = " + viMap.gateToLabel[answers.s_fault] + "\n")
+
+	var timeTotal = madWrapper.getTimeTotal()
+	
+	var indicators = assessment.getIndicatorValues()
+	indicators.put("fault", answers.c_fault == answers.s_fault ? 1 : 0)
+	indicators.put("numChanges", answers.numChanges)
+	indicators.put("timeTotal", timeTotal)
+	
+	log("----------\n")		
+	log(glob.activityLog)
+}
