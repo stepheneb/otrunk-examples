e90be127feec4f6ef1c477cee5133066459f0783
sbannasch <sbannasch@6e01202a-0783-4428-890a-84243c50cc2b>
2008-07-02 17:13:36 +0000

    convert dates to ISO8601 so they sort correctly
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@14120 6e01202a-0783-4428-890a-84243c50cc2b

--- index-builder-script/build-index.rb
+++ index-builder-script/build-index.rb
@@ -55,7 +55,7 @@ otrunk_example_dirs.each do |path|
   svn_props = YAML::load(`svn info #{path}`)
   examples = Dir.glob("#{path}/*.otml").length
   index_page_body += "<tr><td><a href=""#{path}/ot-index.html"">#{path}</a></td>"
-  index_page_body += "<td>#{svn_props["Last Changed Date"][/(.*) -/, 1]}</td>"
+  index_page_body += "<td>#{svn_props["Last Changed Date"][/(.*) -/, 1].gsub(/ /, 'T')}Z</td>"
   index_page_body += "<td>#{examples}</td></tr>\n"
 end
 
