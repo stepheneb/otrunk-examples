73b86620479653ec04acdfb7f9b64bf51e8a2242
skim <skim@6e01202a-0783-4428-890a-84243c50cc2b>
2008-07-28 19:04:25 +0000

    New format for report
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@14477 6e01202a-0783-4428-890a-84243c50cc2b

--- CAPA/capa_multimeter_group_report.js
+++ CAPA/capa_multimeter_group_report.js
@@ -3,6 +3,7 @@ importClass(Packages.java.lang.StringBuffer)
 importClass(Packages.org.concord.otrunk.capa.util.XHTMLTable)
 importClass(Packages.org.concord.otrunk.capa.util.CSVTable)
 importClass(Packages.org.concord.otrunkcapa.OTMultimeterAssessment)
+importClass(Packages.org.concord.otrunkcapa.OTMultimeterAsessmentGradeUtil)
 importClass(Packages.org.concord.otrunkcapa.rubric.RubricGradeUtil)
 
 /*
@@ -10,6 +11,17 @@ importClass(Packages.org.concord.otrunkcapa.rubric.RubricGradeUtil)
  *   otRubric
  *   otSource
  */
+
+// Store for calculation of stats, etc.
+var s = {
+	voltage : { lead : [], circuit : [], score : [] },
+	current : { lead : [], circuit : [], score : [] },
+	resistance : {lead : [], circuit : [], score : [] },
+	total : [],
+	time : [],
+	blowUp : [],
+	color : "#000000"
+} 
  
 function init() {
 	System.out.println("init()")
@@ -27,151 +39,129 @@ function getText() {
 	rubrics[2] = otRubric.getResistanceRubric()
 	var labels = ["voltage", "current", "resistance"]
 	var users = otSource.getUsers()
-	var contentsMap = otSource.getContentsMap()		
-	
-	return "<h3>CAPA Aggregate Report</h3>\n" + 
-		getTableView(rubrics, labels, users, contentsMap) + 
-		"<h3>CSV</h3>\n<pre>\n" + 
-		getCSView(rubrics, labels, users, contentsMap) + 
-		"\n</pre>\n"
-}
-
-function getTableView(rubrics, labels, users, contentsMap) {
-	var table = null
-	var text = new StringBuffer()
-	var header = new XHTMLTable()
-
-	header.set("", 0, 0)
-	header.set("", 0, 1)	
-	var indicators = rubrics[0].getIndicators()
-
-	var col = 2
-	for (var i = 0; i < indicators.size(); ++i) {
-		header.set("<b>" + indicators.get(i).getLabel() + "</b>", 0, col)
-		++col
-	}
-	header.set("<b>Total</b>", 0, col)
-	
-	for (var i = 0; i < users.size(); ++i) {
-		var assessmentPresent = false
-		var user = users.get(i)
-		var name = user.getName()			
-		var contents = contentsMap.get(name)
-
-		text.append("<h3>" + name + "</h3>\n")
-		
-		for (var j = 0; j < contents.size(); ++j) {
-			var content = contents.get(j)
-			if (content instanceof OTMultimeterAssessment) {
-				assessmentPresent = true
-				
-				table = new XHTMLTable()
-				table.set(header, 0, 0)
-				var row = 1
-				var assessmentObjects = content.getAnswers()
-					
-				for (var k = 0; k < assessmentObjects.size(); ++k) {
-					var assessment = assessmentObjects.get(k)
-					var rubric = rubrics[k]
-					indicatorValues = assessment.getIndicatorValues()
-
-					table.set(labels[k], row, 0, 3, 1)
-					table.set("Indicator", row, 1)
-					table.set("Label", row+1, 1)
-					table.set("Points", row+2, 1)
-
-					col = 2
-					
-					for (var m = 0; m < indicators.size(); ++m) { // for each indicator for the assessment
-						var indicator = indicators.get(m)						
-						var indicatorGrade = RubricGradeUtil.getIndicatorGrade(assessment, indicator, rubric)
-						if (indicatorGrade == null) {
-							table.set("NA", row, col)
-							table.set("NA", row+1, col)
-							table.set("NA", row+2, col)
-						}
-						else {
-							table.set("" + indicatorValues.get(indicator.getName()), row, col)
-							table.set("" + indicatorGrade.getOTIndicatorGrade().getLabel(), row+1, col)
-							table.set("" + indicatorGrade.getPoints(), row+2, col)
-						}
-						++col
-					}
-					table.set("" + RubricGradeUtil.getTotalGrade(assessment, rubric).getPoints(), row+2, col)
-					row += 3
-				}
-				text.append(table.getText())
-			}
-		}
-		if (!assessmentPresent) {
-			text.append(name + ": empty" + "<p/>\n")
-		}
-	}
-	return text.toString()
-}
-
-function getCSView(rubrics, labels, users, contentsMap) {
-	var header = new CSVTable()
-	var table = new CSVTable()
-	var row = 0;
-	var col = 0;
-	var indicators = rubrics[0].getIndicators() // same indicators for voltage, current, resistance
-	var assessment = null
-		
-	header.set("Name", 0, 0)
-	header.set("Type", 0, 1)	
-	col = 2
+	var contentsMap = otSource.getContentsMap()
+	var indicatorMap = {}
+	var indicators = rubrics[0].getIndicators() //assuming same set of indicators for rubrics 0..2
 	for (var i = 0; i < indicators.size(); ++i) {
 		var indicator = indicators.get(i)
-		header.set(indicator.getLabel(), row, col)
-		header.set("Indicator", row, col+1)
-		header.set("Points", row, col+2)
-		col += 3
-	}
-	header.set("Total", row, col)
+		indicatorMap[indicator.getName()] = indicator
+	}		
 	
-	table.set(header, 0, 0)
-	++row
-	
+	return "<h3>CAPA Aggregate Report</h3>\n" +	getTableView(rubrics, labels, users, contentsMap, indicatorMap)
+}
+
+function getTableView(rubrics, labels, users, contentsMap, indicatorMap) {
+	var b = new StringBuffer("")
+	b.append("<table border=\"1\">\n")
+	b.append("<tr><td></td><td colspan=\"3\">Voltage</td><td colspan=\"3\">Current</td><td colspan=\"3\">Resistance</td>")
+	b.append("<td rowspan=\"2\">Total</td><td rowspan=\"2\">Time (s)</td><td rowspan=\"2\">Blown Meters</td></tr>")
+	b.append("<tr><td></td><td>Leads</td><td>Circuit</td><td>Score</td><td>Leads</td><td>Circuit</td><td>Score</td><td>Leads</td><td>Circuit</td><td>Score</td>")
 	for (var i = 0; i < users.size(); ++i) {
 		var user = users.get(i)
-		var name = user.getName()
-		var contents = contentsMap.get(name)
-			
-		for (var j = 0; j < contents.size(); ++j) {
-			var content = contents.get(j)
-			
-			if (content instanceof OTMultimeterAssessment) {
-				var assessmentObjects = content.getAnswers();
-				for (var k = 0; k < assessmentObjects.size(); ++k) {
-					var assessment = assessmentObjects.get(k)
-					var indicatorValues = assessment.getIndicatorValues()
-					
-					table.set(name, row, 0)
-					table.set(labels[k], row, 1)
-					col = 2
-					
-					for (var m = 0; m < indicators.size(); ++m) { // for each indicator for the assessment
-						var indicator = indicators.get(m)						
-						var indicatorGrade = RubricGradeUtil.getIndicatorGrade(assessment, indicator, rubrics[k]);
-						if (indicatorGrade == null) {
-							table.set("NA", row, col)
-							table.set("NA", row, col+1)
-							table.set("NA", row, col+2)
-						}
-						else {
-							table.set(indicatorGrade.getOTIndicatorGrade().getLabel(), row, col)
-							table.set("" + indicatorValues.get(indicator.getName()), row, col+1) 
-							table.set("" + indicatorGrade.getPoints(), row, col+2)
-						}
-						col += 3
-					}
-					table.set("" + RubricGradeUtil.getTotalGrade(assessment, rubrics[k]).getPoints(), row, col)
-					++row
-				}
-			}
+		var uname = user.getName()
+		var assessment = getAssessment(contentsMap.get(uname))
+		if (assessment != null) {
+			b.append(getUserLine(uname, assessment, rubrics, indicatorMap))
 		}
 	}
-	return table.getText();
-}	
+	b.append(getSummaryLine())
+	b.append("</table>")
+	return b.toString();
+}
 
+function getAssessment(contents) { 
+	var gotAssessment = false
+	var assessment = null
+	
+	for (var i = 0; i < contents.size(); ++i) {
+		var content = contents.get(i)
+		if (content instanceof OTMultimeterAssessment) {
+			gotAssessment = true
+			assessment = content 
+		}
+	}
+	return assessment //get the last one for the user
+}
+
+function getUserLine(uname, assessment, rubrics, indicatorMap) {
+	var b = new StringBuffer("<tr><td>" + uname + "</td>")
+	subAssessments = assessment.getAnswers()
+	
+	var total_time = 0
+	var total_blowup = 0
+	
+	for (var i = 0; i < subAssessments.size(); ++i) {
+		var subAssessment = subAssessments.get(i)
+		var rubric = rubrics[i]
+		var indicatorValues = subAssessment.getIndicatorValues()
+		
+		var score = RubricGradeUtil.getTotalGrade(subAssessment, rubric).getPoints()
+		total_time += parseFloat(indicatorValues.get("time"))
+		total_blowup += parseInt(indicatorValues.get("brokenDMM"))
+
+		var test = null;
+		if (i == 0) { test = s.voltage; }
+		else if (i == 1) { test = s.current; }
+		else if (i == 2) { test = s.resistance; }
+				
+		var leadInd = parseInt(indicatorValues.get("leadPlacement"))
+		test.lead.push(leadInd > 1 ? 1 : 0)
+		
+		var circuitInd = parseInt(indicatorValues.get("circuitSetting"))
+		test.circuit.push(circuitInd > 0 ? 1 : 0)
+		
+		test.score.push(score)
+			
+		b.append("<td>" + getLabel(indicatorMap["leadPlacement"], subAssessment, rubric) + "</td>")
+		b.append("<td>" + getLabel(indicatorMap["circuitSetting"], subAssessment, rubric) + "</td>")
+		b.append("<td>" + score + "</td>")
+	}
+
+	var totalPoints = OTMultimeterAsessmentGradeUtil.getTotalGrade(assessment, otRubric).getPoints()
+	
+	s.total.push(totalPoints)
+	s.time.push(total_time)
+	s.blowUp.push(total_blowup)
+	
+	b.append("<td><font color=\"#2020f0\"><b>" +  totalPoints + "</b></font></td>")
+	b.append("<td>" + total_time.toFixed(1) + "</td>")
+	b.append("<td>" + total_blowup + "</td>")	
+	
+	return b.toString() + "</tr>\n"
+}
+function getLabel(indicator, assessment, rubric) {
+	var indicatorGrade = RubricGradeUtil.getIndicatorGrade(assessment, indicator, rubric)
+	return indicatorGrade.getOTIndicatorGrade().getLabel()
+}
+
+function getSummaryLine() {
+	var b = new StringBuffer("<tr>")
+	s.color = "#a030a0"
+	b.append(ctd("<b>Average</b>"))
+	b.append(ctd("<b>" + (avg(s.voltage.lead) * 100).toFixed(0) + "%</b>"))
+	b.append(ctd("<b>" + (avg(s.voltage.circuit) * 100).toFixed(0) + "%</b>"))	
+	b.append(ctd("<b>" + avg(s.voltage.score).toFixed(0) + "</b>"))		
+	b.append(ctd("<b>" + (avg(s.current.lead) * 100).toFixed(0) + "%</b>"))	
+	b.append(ctd("<b>" + (avg(s.current.circuit) * 100).toFixed(0) + "%</b>"))
+	b.append(ctd("<b>" + avg(s.current.score).toFixed(0) + "</b>"))
+	b.append(ctd("<b>" + (avg(s.resistance.lead) * 100).toFixed(0) + "%</b>"))		
+	b.append(ctd("<b>" + (avg(s.resistance.circuit) * 100).toFixed(0) + "%</b>"))
+	b.append(ctd("<b>" + avg(s.resistance.score).toFixed(0) + "</b>"))
+	b.append(ctd("<b>" + avg(s.total).toFixed(0) + "</b>"))		
+	b.append(ctd("<b>" + avg(s.time).toFixed(0) + "</b>"))			
+	b.append(ctd("<b>" + avg(s.blowUp).toFixed(2) + "</b>"))		
+	b.append("</tr>")
+	return b.toString()
+}
+
+function ctd(text) {
+	return "<td><font color=\"" + s.color + "\">" + text + "</font></td>" 
+}
+
+function avg(a) {
+	var sum = 0
+	for (var i = 0; i < a.length; ++i) {
+		sum += a[i]
+	}
+	return a.length > 0 ? sum / a.length : 0
+}
