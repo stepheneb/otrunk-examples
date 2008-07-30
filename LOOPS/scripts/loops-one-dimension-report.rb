56b43273763d77d62abc0838aa15efb61a64bebb
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2008-07-30 13:58:38 +0000

    Added report for Carolyn LOOPS activity.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@14531 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ LOOPS/scripts/loops-one-dimension-report.rb
@@ -0,0 +1,73 @@
+require 'erb'
+include_class 'org.concord.framework.otrunk.view.OTUserListService'
+include_class 'org.concord.framework.otrunk.OTrunk'
+include_class 'org.concord.datagraph.state.OTDataGraph'
+include_class 'org.concord.datagraph.state.OTDataGraphable'
+
+def getText
+  @otrunk = $viewContext.getViewService(OTrunk.java_class);
+    
+  erb = ERB.new Java::JavaLang::String.new($template.src).to_s
+  erb.result(binding)   
+end
+
+def users
+  userListService = $viewContext.getViewService(OTUserListService.java_class)
+  userListService.getUserList()
+end
+
+def embedObject(obj)
+  "<object refid=\"#{ obj.otExternalId() }\"/>"
+end
+
+def embedUserObject(obj, user)
+  "<object refid=\"#{ obj.otExternalId() }\" user=\"#{ user.getUserId().toExternalForm() }\"/>"
+end
+
+def hasUserModified(obj, user)
+  @otrunk.hasUserModified(obj, user)
+end
+
+def userObject(obj, user)
+  @otrunk.getUserRuntimeObject(obj, user);
+end
+
+def otCreate(rconstant, &block)
+  otObj = $otObjectService.createObject(rconstant.java_class)
+  yield otObj
+  otObj
+end
+
+def colors
+  ["FF0000", "00FF00", "0000FF", "FFFF00", "FF00FF", "00FFFF"]
+end
+
+def nextColor
+  color = colors[@colorIndex % colors.length].hex
+  @colorIndex += 1
+  color
+end
+
+def multiUserGraph(dataGraph, dataStore)
+  otCreate(OTDataGraph) { |multiUserGraph|
+    multiUserGraph.title = dataGraph.title
+    multiUserGraph.showGraphableList=true
+    multiUserGraph.graphableListEditable=false
+    multiUserGraph.xDataAxis = dataGraph.xDataAxis
+    multiUserGraph.yDataAxis = dataGraph.yDataAxis
+    @colorIndex = 0
+    graphables = multiUserGraph.graphables
+    users.each { |user|
+      next unless hasUserModified(dataStore,user)
+      graphables.add(otCreate(OTDataGraphable) { | graphable |
+        graphable.name = user.name
+        graphable.color = nextColor
+        graphable.xColumn=0
+        graphable.yColumn=1
+		graphable.locked=true
+		graphable.drawMarks=false
+		graphable.dataStore = userObject(dataStore,user)
+      })
+    }  
+  }   
+end
