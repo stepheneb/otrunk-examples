86c78b24ab8ddeb724a865fc0b3c9e7e3e529b7f
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2008-08-05 05:36:00 +0000

    Initial UDLReport check in.  This doesn't work yet, and might rely on some Java code which isn't checked in yet.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@14676 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ UDLReport/scripts/udl-multipage-report.rb
@@ -0,0 +1,112 @@
+require 'erb'
+include_class 'org.concord.framework.otrunk.view.OTUserListService'
+include_class 'org.concord.framework.otrunk.OTrunk'
+include_class 'org.concord.datagraph.state.OTDataGraph'
+include_class 'org.concord.datagraph.state.OTDataGraphable'
+include_class 'org.concord.otrunk.script.ui.OTScriptVariable'
+include_class 'org.concord.framework.otrunk.wrapper.OTString'
+
+def getText
+  @otrunk = $viewContext.getViewService(OTrunk.java_class);
+  if $action
+    actionStr = $action.string
+  else
+    actionStr = "default_template"
+  end
+  eval(actionStr)
+end
+
+def default_template
+  render $template
+end
+
+def page2
+  render $template_page2
+end
+
+def render(templateBlob)
+  erb = ERB.new Java::JavaLang::String.new(templateBlob.src).to_s
+  erb.result(binding)   
+end 
+
+def embedObject(obj)
+  "<object refid=\"#{ obj.otExternalId() }\"/>"
+end
+
+def otCreate(rconstant, &block)
+  otObj = $otObjectService.createObject(rconstant.java_class)
+  yield otObj
+  otObj
+end
+
+def linkToObject(link_text, obj)
+  "<a href=\"#{ obj.otExternalId() }\">#{link_text}</a>"
+end
+
+def linkToObjectAction(link_text, obj, action)
+  viewEntryCopy = $otObjectService.copyObject($viewEntry, 1)
+  viewEntryCopy.variables.add(otCreate(OTScriptVariable){|scriptVar|
+    scriptVar.name="action"
+    scriptVar.reference = otCreate(OTString){|str|
+      str.string=action
+    }
+  })
+  "<a href=\"#{ obj.otExternalId() }\" viewid=\"#{viewEntryCopy.otExternalId()}\">#{link_text}</a>"
+end
+
+def users
+  userListService = $viewContext.getViewService(OTUserListService.java_class)
+  userListService.getUserList()
+end
+
+def embedUserObject(obj, user)
+  "<object refid=\"#{ obj.otExternalId() }\" user=\"#{ user.getUserId().toExternalForm() }\"/>"
+end
+
+def hasUserModified(obj, user)
+  @otrunk.hasUserModified(obj, user)
+end
+
+def userObject(obj, user)
+  @otrunk.getUserRuntimeObject(obj, user);
+end
+
+def otCreate(rconstant, &block)
+  otObj = $otObjectService.createObject(rconstant.java_class)
+  yield otObj
+  otObj
+end
+
+def colors
+  ["65AE24", "E08024", "905BF5", "568AD8", "B03A7F", "99A62A"]
+end
+
+def nextColor
+  color = colors[@colorIndex % colors.length].hex
+  @colorIndex += 1
+  color
+end
+
+def multiUserGraph(dataGraph, dataStore)
+  otCreate(OTDataGraph) { |multiUserGraph|
+    multiUserGraph.title = dataGraph.title
+    multiUserGraph.showGraphableList=true
+    multiUserGraph.graphableListEditable=false
+    multiUserGraph.xDataAxis = dataGraph.xDataAxis
+    multiUserGraph.yDataAxis = dataGraph.yDataAxis
+    @colorIndex = 0
+    graphables = multiUserGraph.graphables
+    users.each { |user|
+      next unless hasUserModified(dataStore,user)
+      graphables.add(otCreate(OTDataGraphable) { | graphable |
+        graphable.name = user.name
+        graphable.color = nextColor
+        graphable.xColumn=0
+        graphable.yColumn=1
+        graphable.locked=true
+        graphable.drawMarks=false
+        graphable.dataStore = userObject(dataStore,user)
+      })
+    }  
+  }   
+end
