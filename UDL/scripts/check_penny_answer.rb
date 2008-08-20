974abadb51023ee91c9dc672133d6fcf1546cf56
sbannasch <sbannasch@6e01202a-0783-4428-890a-84243c50cc2b>
2008-08-20 17:39:12 +0000

    updated check_penny_answer ruby script, changed OTSmartGraph references to OTSmartGraphTool
    The check_penny_answer now has a module: 
    
      OTrunkRubyScriptTools 
    
    and a class: 
    
      LabelRangeResponse 
    
    which in the future could in separate script library files.
    
    I also updated OTViewer_UDLSelected launcher to reference all needed projects
    
    
    
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@14951 6e01202a-0783-4428-890a-84243c50cc2b

--- UDL/scripts/check_penny_answer.rb
+++ UDL/scripts/check_penny_answer.rb
@@ -1,53 +1,85 @@
 require 'java'
 
-include_class 'javax.swing.JOptionPane'
+puts "ruby script: 'check_penny_answer.rb' evaluated"
 
-def self.init
-  puts "self.init called"
-end
+module OTrunkRubyScriptTools
 
-def self.clicked
-  correct_range = 5..16
-  response_key = {
-    :no_label_entered => { :text => "Use the label tool to add a label to the graph!" },
-    :correct => 
-      { :text => "That's correct!\nNow you can move on to the next page", 
-        :hightlight_region => false },
-    :first_wrong_answer => 
-      { :text => "Oops, that's not correct.\nWhere is the line going slowly upwards?" },
-    :second_wrong_answer => 
-      { :text => "Oops, that's not correct.\nThe area where the line is going slowly upwards in now marked.\nTry adding a label in that region.", 
-        :hightlight_region => true },
-    :multiple_wrong_answers => 
-      { :text => "Oops, that's not correct.\nThe area where the line is going slowly upwards in now marked.\nTry adding a label in that region.", 
-        :hightlight_region => true }
-  }
-  @label_range_response = LabelRangeResponse.new(correct_range, response_key)
-  @label_range_response.clicked
+  include_class 'javax.swing.JOptionPane'
+
+  # displays message in dialog
+  def show_message(message)
+    JOptionPane.showMessageDialog(nil, message)
+  end
+  
+  # looks for the last label entered by the learner
+  # returns nil if no labels have been enetered
+  # or if there are no valid labels
+  # 
+  # extra code is related to a bug where multiple invalid labels are created
+  #
+  # pass in the labels object from an OTDataCollector
+  #
+  # either returns the most recently added valid OTLabel object for the OTDataCollector
+  # or nil if there are no Labels or all the Labels are invalid
+  def last_valid_label(labels)
+    if labels.size == 0
+      nil
+    else
+      label_array = labels.vector.to_a.reverse
+      label_array.each {|l| puts "label: #{l.getX}, #{l.getY}"}
+      label_array.find { |l| l.getX < 100000 }
+    end
+  end
+
 end
 
 class LabelRangeResponse
+  include OTrunkRubyScriptTools
   
-  attr_reader :correct_range, :response_key
+  attr_reader :correct_range, :response_key, :ot_graph, :ot_smart, :ot_correct, :ot_times_incorrect
   
-  def initialize(correct_range, response_key)
+  def initialize(ot_graph, ot_smart, ot_correct, ot_times_incorrect, correct_range, response_key)
     @correct_range = correct_range
     @response_key = response_key
+    @ot_graph = ot_graph
+    @ot_smart = ot_smart
+    @ot_correct = ot_correct
+    @ot_times_incorrect = ot_times_incorrect
+  end
+  
+  # Within a class deﬁnition, Ruby will parse 'correct ='
+  # as an assignment to a local variable named 'correct'.
+  #
+  # So within the class definition
+  def correct=(value)
+    @ot_correct.setValue(value)
+  end
+  
+  def correct
+    @ot_correct.getValue
+  end
+
+  def times_incorrect=(value)
+    @ot_times_incorrect.setValue(value)
+  end
+  
+  def times_incorrect
+    @ot_times_incorrect.getValue
   end
   
   def clicked
-    unless lastLabel = getLastValidLabel($graph.getLabels)
-      $correct.setValue(false)
+    unless lastLabel = last_valid_label(ot_graph.getLabels)
+      self.correct = false
       update(@response_key[:no_label_entered])
     else
       x = lastLabel.getXData
-      if checkConditionWithParamaters(x, correct_range)
-        $correct.setValue(true)
+      if check_condition_with_paramaters(x, correct_range)
+        self.correct = true
         update(@response_key[:correct])
       else
-        $correct.setValue(false)
-        $times_incorrect.setValue($times_incorrect.getValue + 1)
-        case $times_incorrect.getValue
+        self.correct = false
+        self.times_incorrect += 1
+        case times_incorrect
         when 1
           update(@response_key[:first_wrong_answer])
         when 2
@@ -60,42 +92,51 @@ class LabelRangeResponse
   end
 
   def update(key)
-    updateRegionHighlighting(key[:hightlight_region])
-    showMessage(key[:text])
+    update_region_highlighting(key[:hightlight_region])
+    show_message(key[:text])
   end
   
-  def updateRegionHighlighting(highlight_state)
+  def update_region_highlighting(highlight_state)
     case highlight_state
     when true
-      $smart.showRegionLabel(@correct_range.first, @correct_range.last)
+      ot_smart.showRegionLabel(@correct_range.first, @correct_range.last)
     when false
-      $smart.removeAllRegionLabels      
+      ot_smart.removeAllRegionLabels      
     end
   end
-end
-
-# returns true if value is within range
-# returns false otherwise
-def checkConditionWithParamaters(value, range)
-  range.include?(value)
-end
-
-# displays message in dialog
-def showMessage(message)
-  JOptionPane.showMessageDialog(nil, message)
-end
-
-# looks for the last label entered by the learner
-# returns nil if no labels have been enetered
-# or if there are no valid labels
-# 
-# extra code is related to a bug where multiple invalid labels are created
-def getLastValidLabel(labels)
-  if labels.size == 0
-    nil
-  else
-    label_array = labels.vector.to_a.reverse
-    label_array.each {|l| puts "label: #{l.getX}, #{l.getY}"}
-    label_array.find { |l| l.getX < 100000 }
+  
+  # returns true if value is within range
+  # returns false otherwise
+  def check_condition_with_paramaters(value, range)
+    range.include?(value)
   end
 end
+
+def self.init
+  # This is currently not called when the script is run from an OTScriptButton object.
+  # The init method is only called when a script is run from an OTScriptObject onject.
+  puts "self.init called"
+end
+
+def self.clicked
+  puts "times_incorrect: #{@label_range_response.times_incorrect}, class: #{@label_range_response.times_incorrect.class}"
+  puts "times_incorrect + 1: #{@label_range_response.times_incorrect + 1}"
+  @label_range_response.clicked
+end
+
+correct_range = 5..16
+response_key = {
+  :no_label_entered => { :text => "Use the label tool to add a label to the graph!" },
+  :correct => 
+    { :text => "That's correct!\nNow you can move on to the next page", 
+      :hightlight_region => false },
+  :first_wrong_answer => 
+    { :text => "Oops, that's not correct.\nWhere is the line going slowly upwards?" },
+  :second_wrong_answer => 
+    { :text => "Oops, that's not correct.\nThe area where the line is going slowly upwards in now marked.\nTry adding a label in that region.", 
+      :hightlight_region => true },
+  :multiple_wrong_answers => 
+    { :text => "Oops, that's not correct.\nThe area where the line is going slowly upwards in now marked.\nTry adding a label in that region.", 
+      :hightlight_region => true }
+}
+@label_range_response = LabelRangeResponse.new($graph, $smart, $correct, $times_incorrect, correct_range, response_key)
