9c1e64de6beb97767ef045d6143aeccba8b79ef8
sbannasch <sbannasch@6e01202a-0783-4428-890a-84243c50cc2b>
2008-09-02 19:40:36 +0000

    added highlight => false to no_answer and first_wrong_answer
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@15125 6e01202a-0783-4428-890a-84243c50cc2b

--- UDL/scripts/smart_graph_label_script_template.rb
+++ UDL/scripts/smart_graph_label_script_template.rb
@@ -42,18 +42,16 @@ response_key = {
   
   # Optional: specify the highlight_range if it is different than the correct_range
   # :highlight_range => { :range => 5..30, :axis => :x },
-  :no_answer_entered => { :text => "Use the label tool to add a label to the graph." },
+  :no_answer_entered => 
+    { :text => "Use the label tool to add a label to the graph.", :hightlight_region => false  },
   :correct => 
-    { :text => "Correct!\nGo on to the next page.", 
-      :hightlight_region => false },
+    { :text => "Correct!\nGo on to the next page.", :hightlight_region => false },
   :first_wrong_answer => 
-    { :text => "That's not correct.\n" },
+    { :text => "That's not correct.\n", :hightlight_region => false  },
   :second_wrong_answer => 
-    { :text => "That's still not correct.\n", 
-      :hightlight_region => true },
+    { :text => "That's still not correct.\n", :hightlight_region => true },
   :multiple_wrong_answers => 
-    { :text => "That's still not correct.\n", 
-      :hightlight_region => true }
+    { :text => "That's still not correct.\n", :hightlight_region => true }
 }
 
 # Create a new SmartGraphRangeResponse object with your response_key and the 
