951bad49051d83dfd41080ac8adc168f20b18e31
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2008-09-24 19:06:29 +0000

    add wrap-up script
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@15571 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ UDL/scripts/wrap-up_script.js
@@ -0,0 +1,39 @@
+importClass(Packages.org.concord.otrunk.ui.OTCurriculumUnitHelper)
+importClass(Packages.javax.swing.JOptionPane)
+
+function clicked(){
+	if (isPasswordCorrect()){
+		unlockPostTestAndExit()
+	} else {
+		showMessage("That's not the correct password.")
+	}
+}
+
+function isPasswordCorrect(){
+	return passwordField.getText().equalsIgnoreCase(password.getText())
+}
+
+function unlockPostTestAndExit(){
+	var curnitHelper = OTCurriculumUnitHelper.getActivityHelper(otObjectService)
+	var sectionHolder = curnitHelper.getSectionsContainer()
+	unlockPostTest(sectionHolder)
+	sectionHolder.setCurrentCard(sectionHolder.getCards().get(0))
+}
+
+function unlockPostTest(sectionHolder){
+	for(var i=0; i<sectionHolder.getCards().size(); i++) {
+		activity = sectionHolder.getCards().get(i)
+		if (activity.getIsPosttest()){
+			activity.setEnabled(true)
+		} else {
+			activity.setEnabled(false)
+		}
+	}
+}
+
+function showMessage(message){
+	JOptionPane.showMessageDialog(null,
+						    message,
+						    "Notice",
+						    JOptionPane.INFORMATION_MESSAGE);
+}
