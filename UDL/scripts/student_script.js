cbe8e23fbaff54180a515b9d1cfddcf5cf50fbb7
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2008-09-25 19:34:59 +0000

    add new questions to script
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@15622 6e01202a-0783-4428-890a-84243c50cc2b

--- UDL/scripts/student_script.js
+++ UDL/scripts/student_script.js
@@ -293,4 +293,6 @@ var otids = ["e07ec17d-e3c8-11dc-ac60-9facaf86308a",
 		"93da4320-0d8c-11dd-91fa-c12e917b8f18",
 		"21e76cfb-25eb-11dd-8aea-49a3dbad775f",	
 		"debf0aee-25ea-11dd-8aea-49a3dbad775f",	
-		"69b7f136-2c24-11dd-a948-0d59c68f631b"]
+		"69b7f136-2c24-11dd-a948-0d59c68f631b",
+		"51af985b-7cd6-11dc-ab8f-f5a7386e7836",
+		"678b5190-0bcf-012a-b542-0017f2cc694e!/act2-elab-ques1"]
