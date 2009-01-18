748ca7c25a9e9667f3356e1881a83b8699222dc6
sbannasch <sbannasch@6e01202a-0783-4428-890a-84243c50cc2b>
2009-01-18 07:08:18 +0000

    one more fix in index-builder.rb
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@16696 6e01202a-0783-4428-890a-84243c50cc2b

--- index-builder-script/build-index.rb
+++ index-builder-script/build-index.rb
@@ -166,7 +166,7 @@ HERE
   <td width=260><a href=#{otml_url}>#{filename}</a></td>
 HEREDOC
     end
-    if svn_rev2.empty? 
+    if svn_rev2 && svn_rev2.empty?
       otml_launchers += "<td width=180>not in svn</td></tr>"
     else
       otml_launchers += "<td width=180><a href=#{trac_otml_url}>#{svn_rev2}</a></td></tr>"
