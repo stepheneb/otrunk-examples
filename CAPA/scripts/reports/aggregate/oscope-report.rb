09bb0ef07c8ecf7cfaf1526b4f6c281983ce7c5d
skim <skim@6e01202a-0783-4428-890a-84243c50cc2b>
2009-01-22 22:46:16 +0000

    Changed format of csv report.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@16769 6e01202a-0783-4428-890a-84243c50cc2b

--- CAPA/scripts/reports/aggregate/oscope-report.rb
+++ CAPA/scripts/reports/aggregate/oscope-report.rb
@@ -1,5 +1,5 @@
 ## Aggregate report for performance assessment "Using the Oscilloscope"
- 
+
 require 'jruby'
 require 'erb'
 
@@ -12,14 +12,14 @@ include_class 'org.concord.otrunkcapa.rubric.RubricGradeUtil'
 def getText
   @debug = true  
   init 
-  
+
   if $action
     actionStr = $action.string
   else
     actionStr = '@controller.defaultTemplate'
   end
-  
-  return eval(actionStr)
+
+  eval(actionStr)
 end
 
 def init
@@ -33,10 +33,10 @@ def init
   @questions = Questions.new(@otrunkHelper)
   @assessment = Assessment.new(@otrunkHelper)
   @controller = Controller.new(binding) #erb templates will be evaluated in current binding
-  
+
   @sep = '|' #field separator in csv text 
   @newline = "\n"
-  
+
   @sep = '|' #field separator in csv text 
   @newline = "\n"
 end
@@ -47,7 +47,7 @@ def otImport(script)
       srcValue = script.otGet(srcProp)
       eval(Java::JavaLang::String.new(script.src).to_s, nil, srcValue.getBlobURL().toExternalForm())
   else
-    System.err.println("Cannot import #{script}")    
+    System.err.println("Cannot import #{script}")
   end
 end
 
@@ -56,81 +56,78 @@ def parseLog(user)
   @correctFrq = '-'
   @submittedAmp = '-'
   @submittedFrq = '-'
-  
+
   log = @otrunkHelper.getLastContent(user, OTText).getText
   n = 0
-  
+
   for line in log
     case line
-      when /Correct amplitude = (\S+\s+\S+)/; @correctAmp = Regexp.last_match(1); n += 1       
-      when /Correct frequency = (\S+\s+\S+)/; @correctFrq = Regexp.last_match(1); n += 1       
-      when /submitted: amplitude = (\S+\s+\S+)/; @submittedAmp = Regexp.last_match(1); n += 1       
-      when /submitted: frequency = (\S+\s+\S+)/; @submittedFrq = Regexp.last_match(1); n += 1               
+      when /Correct amplitude = (\S+\s+\S+)/; @correctAmp = Regexp.last_match(1); n += 1
+      when /Correct frequency = (\S+\s+\S+)/; @correctFrq = Regexp.last_match(1); n += 1
+      when /submitted: amplitude = (\S+\s+\S+)/; @submittedAmp = Regexp.last_match(1); n += 1
+      when /submitted: frequency = (\S+\s+\S+)/; @submittedFrq = Regexp.last_match(1); n += 1
     end
     
     break if n > 3
   end
 end
-  
+
 def getCsvText
   t = ''
   t << getCsvHeader
-  
+
   @indicatorMap = {} 
-  for indicator in $rubric.getIndicators.getVector 
+  for indicator in $rubric.getIndicators.getVector
     @indicatorMap[indicator.getName] = indicator
   end 
-    
+
   for user in @otrunkHelper.users
     assessment = @assessment.getLastAssessment(user)
-    if assessment == nil 
+    if assessment == nil
       Util.error("Assessment not present for user [#{user.getName}]")
       next
     end
     parseLog(user)
     t << getCsvUserLine(user, assessment, $rubric)
   end
-  return t
+  t
 end
 
 def getCsvHeader
   indicators = $rubric.getIndicators.getVector
-  t = ''
-  # First line
-  t << @sep * 6
+
+  t = ['Teacher', 'Class', 'First Name', 'Last Name', 'Activity Name'].join(@sep) + @sep
+  t << 'Correct Answer' + @sep + 'Submitted Answer' +  @sep
+
+  t << 'Correct Amplitude' + @sep + 'Submitted Amplitude' + @sep
+  t << 'Correct Frequency' + @sep + 'Submitted Frequency' + @sep
+
   for indicator in indicators
-    t << indicator.getName + '->' + @sep * 3
-  end 
-  t << @newline
-  
-  # Second line
-  t << 'First Name' + @sep + 'Last Name' + @sep
-  t << 'Submitted Amplitude' << @sep << 'Correct Amplitude' << @sep
-  t << 'Submitted Frequency' << @sep << 'Correct Frequency' << @sep
-  for indicator in indicators
-    t << 'String' + @sep + 'Indicator' + @sep + "Points (#{RubricGradeUtil.getMaximumPoints(indicator)})" + @sep
+    t << "Max Points #{indicator.getName}"+ @sep
+    t << "Points" + @sep
   end
-  t << "Final Grade (#{RubricGradeUtil.getTotalMaximumPoints($rubric)})" + @newline
-  return t
+  t << "Max Final Grade" << @sep
+  t << "Final Grade" + @newline
 end
 
 def getCsvUserLine(user, assessment, rubric)
-  t = ''
-  
+  t = @otrunkHelper.teacherName << @sep
+  t << @otrunkHelper.className << @sep
   name = user.getName.split
-  t <<  (name.size > 1 ? name[0] : '') + @sep + name[-1] + @sep
-  t << @submittedAmp << @sep << @correctAmp << @sep
-  t << @submittedFrq << @sep << @correctFrq << @sep
-    
+  t << (name.size > 1 ? name[0] : '') + @sep + name[-1] + @sep
+  t << @otrunkHelper.activityName << @sep
+
+  t << @correctAmp + @sep + @submittedAmp + @sep
+  t << @correctFrq + @sep + @submittedFrq + @sep
+
   indicators = rubric.getIndicators.getVector
-  indicatorValues = assessment.getIndicatorValues  
-  
+  indicatorValues = assessment.getIndicatorValues
+
   for indicator in indicators
     name = indicator.getName
-    t << @assessment.getIndicatorLabel(@indicatorMap[name], assessment, rubric) + @sep
-    t << indicatorValues.get(name).to_s + @sep
+    t << RubricGradeUtil.getMaximumPoints(indicator).to_s + @sep
     t << @assessment.getIndicatorPoints(@indicatorMap[name], assessment, rubric).to_s + @sep
   end
+  t << RubricGradeUtil.getTotalMaximumPoints(rubric).to_s + @sep
   t << RubricGradeUtil.getTotalGrade(assessment, rubric).getPoints.to_s + @newline
-  return t        
 end
