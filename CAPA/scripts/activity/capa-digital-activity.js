eee04bd72f05ef5bf06043721277242e0baddf42
skim <skim@6e01202a-0783-4428-890a-84243c50cc2b>
2009-02-19 23:18:28 +0000

    Removed redundant code + some minor beautifications
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@17284 6e01202a-0783-4428-890a-84243c50cc2b

--- CAPA/scripts/activity/capa-digital-activity.js
+++ CAPA/scripts/activity/capa-digital-activity.js
@@ -1,48 +1,48 @@
-importClass(Packages.java.lang.System)
-importClass(Packages.java.lang.Integer)
-importClass(Packages.java.lang.Float)
-importClass(Packages.java.lang.Double)
-importClass(Packages.java.text.SimpleDateFormat)
 importClass(Packages.java.awt.Color)
 importClass(Packages.java.awt.event.ActionListener)
-importClass(Packages.javax.swing.JOptionPane)
+importClass(Packages.java.lang.Double)
+importClass(Packages.java.lang.Float)
+importClass(Packages.java.lang.Integer)
+importClass(Packages.java.lang.System)
 importClass(Packages.java.math.RoundingMode)
+importClass(Packages.java.text.SimpleDateFormat)
+
+importClass(Packages.javax.swing.JOptionPane)
 
 importClass(Packages.org.concord.framework.otrunk.view.OTUserListService)
+importClass(Packages.org.concord.otrunk.labview.DTSAssessmentUtil)
+importClass(Packages.org.concord.otrunk.labview.LabviewMonitor)
+importClass(Packages.org.concord.otrunk.labview.LabviewReportConverter)
+importClass(Packages.org.concord.otrunk.labview.MADWrapper)
 importClass(Packages.org.concord.otrunk.ui.OTText)
 importClass(Packages.org.concord.otrunk.ui.swing.OTCardContainerView)
 importClass(Packages.org.concord.otrunkcapa.rubric.OTAssessment)
 importClass(Packages.org.concord.otrunkcapa.rubric.OTAssessmentView)
 importClass(Packages.org.concord.otrunkcapa.rubric.RubricGradeUtil)
-importClass(Packages.org.concord.otrunk.labview.LabviewMonitor)
-importClass(Packages.org.concord.otrunk.labview.LabviewReportConverter)
-importClass(Packages.org.concord.otrunk.labview.MADWrapper)
-
 
 /*
- * Variables from OTScriptContextHelper
- * ====================================
- * otContents
- * viewContext
- * controllerService
+ * Variables from context 
  */
- 
-/*
- * Variables from otml 
- * otc_ : component variables
- */
- 
-var ot_monitor
-var ot_cards
- 	
-var glob = {
-	dateFormat : SimpleDateFormat.getInstance(),
-	currentStep : 1,
-	lastStep : 1,
-	monitor : null, // "real object" for ot_monitor
-	otAssessment : null,
-	madWrapper : null,
-	activityLog : ""
+var c = {
+    // from OTScriptContextHelper
+    otContents : otContents,
+    viewContext : viewContext,
+    controllerService : controllerService,
+    // from otml
+    assessment : ot_assessment,
+    monitor : ot_monitor,
+    cards : ot_cards
+}
+
+// Other global variables
+var g = {
+    dateFormat : SimpleDateFormat.getInstance(),
+    currentStep : 1,
+    lastStep : 1,
+    monitor : null, // "real object" for ot_monitor
+    madWrapper : null,
+    activityLog : "",
+    helper : new DTSAssessmentUtil()
 }
 
  /**
@@ -51,56 +51,55 @@ var glob = {
  * was successful or not.
  */
 function init() {
-	System.out.println("Entered: init()")
-	glob.dateFormat.applyPattern("MM/dd/yyyy HH:mm:ss zzz");
-    glob.monitor = controllerService.getRealObject(ot_monitor)
-    glob.monitor.setExitListener(listeners.labviewExitListener)
+    System.out.println("Entered: init()")
+    g.dateFormat.applyPattern("MM/dd/yyyy HH:mm:ss zzz");
+    g.monitor = c.controllerService.getRealObject(c.monitor)
+    g.monitor.setExitListener(listeners.labviewExitListener)
     setupAssessmentLogging()
-	OTCardContainerView.setCurrentCard(ot_cards, "main_card_1")	
-	return true
+    OTCardContainerView.setCurrentCard(c.cards, "main_card_1")    
+    return true
 }
 
 function save() {
-	System.out.println("Entered: save()")
-	return true
+    System.out.println("Entered: save()")
+    return true
 }
 
 function setupAssessmentLogging() {
-	var userName = getUserName()
-	var ms = new Date().getTime()
-	var assessment = ot_assessment
-	assessment.setUserName(userName)
-	assessment.setTime(ms)	
-	otContents.add(assessment)
-	glob.otAssessment = assessment
+    var userName = getUserName()
+    var ms = new Date().getTime()
+    var assessment = c.assessment
+    assessment.setUserName(userName)
+    assessment.setTime(ms)    
+    c.otContents.add(assessment)
 }
 
 function getUserName() {
-	var userListService = viewContext.getViewService(OTUserListService)
-	var users = userListService.getUserList()
-	if (users.size() < 1) {
-		return "A student"
-	}
-	else {
-		return users.get(0).getName()
-	}
+    var userListService = c.viewContext.getViewService(OTUserListService)
+    var users = userListService.getUserList()
+    if (users.size() < 1) {
+        return "A student"
+    }
+    else {
+        return users.get(0).getName()
+    }
 }
 
 function wrap_assess() {
-	var converter = new LabviewReportConverter(glob.monitor)
-	converter.markEndTime()
-	glob.madWrapper = converter.getMADWrapper()
-	var inventory = glob.otAssessment.getInventory()
-	var madID = converter.getOTModelActivityData().getGlobalId()
-	inventory.put("modelActivityData", madID)
-	assess(glob.otAssessment, glob.madWrapper)
+    var converter = new LabviewReportConverter(g.monitor)
+    converter.markEndTime()
+    g.madWrapper = converter.getMADWrapper()
+    var inventory = c.assessment.getInventory()
+    var madID = converter.getOTModelActivityData().getGlobalId()
+    inventory.put("modelActivityData", madID)
+    assess(c.assessment, g.madWrapper)
 }
 
 var listeners = {
-	labviewExitListener : new LabviewMonitor.ExitListener({
-		exited: function() {
-	    	wrap_assess() // must close labVIEW before assess()			
-			OTCardContainerView.setCurrentCard(ot_cards, "main_card_2")				
-		}	
-	})
+    labviewExitListener : new LabviewMonitor.ExitListener({
+        exited: function() {
+            wrap_assess() // must close labVIEW before assess()            
+            OTCardContainerView.setCurrentCard(c.cards, "main_card_2")                
+        }    
+    })
 }
--- CAPA/scripts/activity/capa-digital-assess.js
+++ CAPA/scripts/activity/capa-digital-assess.js
@@ -1,238 +1,217 @@
 // Holder for answers
 var answers =  {
-	c_fault1 : 0, //correct location of fault #1
-	s_fault1 : 0, // submitted location of fault #1
-	c_fault2 : 0, // correct location of fault #2	
-	s_fault2 : 0, // submitted location of fault #2
-	c_fault3 : 0, // correct location of fault #3	
-	s_fault3 : 0, // submitted location of fault #3
-	s_truthValues : "", // submitted answer to truth table question
-	c_truthValues : "10100100", // correct answer to truth table question
-	numHiddenProbes : 0, // number of useless probes
-	numEvidentProbes : 0, // number of useful probes
-	numChanges : 0, // number of changes (input switch flips + probes)
-	time : 0 // total time taken
-}
-
-// Maps used to interpret the data from the LabVIEW VI
-// fault: a number that identifies a fault
-// gate: a number that identifies a gate
-// For example, if fault id is 1, we can find by faultToGate[1] = 3 that gate 3 is faulty,
-// then by looking up gateToLabel[3] find that it means inverter U1C.
-var viMap = {
-	gateToLabel : [
-		"No answer", "Inverter U1A", "Inverter U1B", "Inverter U1C", "AND gate U2A", "AND gate U2B", 
-		"AND gate U3B", "OR gate U3A", "OR gate U3B", "No faults"		
-	],
-	probeMap : [ "No Connection", "U1A Input", "U1A Output", "U1B Input", "U1B Output",
-		"U1C Input", "U1C Output", "U1 Ground", "U1 VCC", "U2A Input1",
-		"U2A Input 2", "U2A Input 3", "U2A Output", "U2B Input 1", "U2B Input 2",
-		"U2B Input 3", "U2B Output", "U2C Input 1", "U2C Input 2", "U2C Input 3",
-		"U2C Output", "U2 Ground", "U2 VCC", "U3A Input 1", "U3A Input 2",
-		"U3A Output", "U3B Input 1", "U3B Input 2", "U3B Output", "U3 Ground",
-		"U3 VCC"
-	],
+    c_fault1 : 0, //correct location of fault #1
+    s_fault1 : 0, // submitted location of fault #1
+    c_fault2 : 0, // correct location of fault #2
+    s_fault2 : 0, // submitted location of fault #2
+    c_fault3 : 0, // correct location of fault #3
+    s_fault3 : 0, // submitted location of fault #3
+    s_truthValues : "", // submitted answer to truth table question
+    c_truthValues : "10100100", // correct answer to truth table question
+    numHiddenProbes : 0, // number of useless probes
+    numEvidentProbes : 0, // number of useful probes
+    numChanges : 0, // number of changes (input switch flips + probes)
+    time : 0 // total time taken
 }
 
 /**
  * This function mut be called AFTER the LabVIEW has exited, and AFTER validateAnswers().
  */
 function assess(assessment, madWrapper) {
-	System.out.println("ENTER: assess()")
-	var inv = glob.otAssessment.getInventory()
+    System.out.println("ENTER: assess()")
+    var inv = c.assessment.getInventory()
 
-	processUserEvents(madWrapper)
-	answers.c_fault1 = parseInt(madWrapper.getLastCIValue("faultLocation1")) 
-	answers.s_fault1 = parseInt(madWrapper.getLastCIValue("answer1")) 
-	answers.c_fault2 = parseInt(madWrapper.getLastCIValue("faultLocation2"))
-	answers.s_fault2 = parseInt(madWrapper.getLastCIValue("answer2")) 
-	answers.c_fault3 = parseInt(madWrapper.getLastCIValue("faultLocation3"))
-	answers.s_fault3 = parseInt(madWrapper.getLastCIValue("answer3")) 
-	answers.time = madWrapper.getTimeTotal()
-	
-	var ttInd = parseInt(madWrapper.getLastCIValue("truthTableCorrect"))
-	var faultInd1 = answers.c_fault1 == answers.s_fault1 ? 1 : 0
-	var faultInd2 = answers.c_fault2 == answers.s_fault2 ? 1 : 0
-	var faultInd3 = answers.c_fault3 == answers.s_fault3 ? 1 : 0	
-	//var numProbesInd = getNumProbesIndicator(madWrapper, faultInd1, faultInd2, faultInd3, answers.numChanges)
-	var timeInd = getTimeIndicator(answers.time, ttInd, faultInd1, faultInd2, faultInd3)
-	
-	var indicators = assessment.getIndicatorValues()
-	indicators.put("truthTable", ttInd)
-	indicators.put("fault1", faultInd1)
-	indicators.put("fault2", faultInd2)	
-	indicators.put("fault3", faultInd3)		
-	indicators.put("uselessProbes", answers.numHiddenProbes)
-	indicators.put("usefulProbes", answers.numEvidentProbes)
-	indicators.put("timeTotal", timeInd)
-	
-	inv.put("c_truthValues", answers.c_truthValues) //save correct answer
-	inv.put("s_truthValues", getSubmittedTruthValuesString(glob.madWrapper)) //save submitted answer
-	inv.put("c_fault1", answers.c_fault1)
-	inv.put("s_fault1", answers.s_fault1)
-	inv.put("c_fault2", answers.c_fault2)
-	inv.put("s_fault2", answers.s_fault2)	
-	inv.put("c_fault3", answers.c_fault3)
-	inv.put("s_fault3", answers.s_fault3)
-	inv.put("timeTotal", answers.time)
-	inv.put("activityLog", glob.activityLog)	
+    processUserEvents(madWrapper)
+    answers.c_fault1 = parseInt(madWrapper.getLastCIValue("faultLocation1")) 
+    answers.s_fault1 = parseInt(madWrapper.getLastCIValue("answer1")) 
+    answers.c_fault2 = parseInt(madWrapper.getLastCIValue("faultLocation2"))
+    answers.s_fault2 = parseInt(madWrapper.getLastCIValue("answer2")) 
+    answers.c_fault3 = parseInt(madWrapper.getLastCIValue("faultLocation3"))
+    answers.s_fault3 = parseInt(madWrapper.getLastCIValue("answer3")) 
+    answers.time = madWrapper.getTimeTotal()
+
+    var ttInd = parseInt(madWrapper.getLastCIValue("truthTableCorrect"))
+    var faultInd1 = answers.c_fault1 == answers.s_fault1 ? 1 : 0
+    var faultInd2 = answers.c_fault2 == answers.s_fault2 ? 1 : 0
+    var faultInd3 = answers.c_fault3 == answers.s_fault3 ? 1 : 0    
+    //var numProbesInd = getNumProbesIndicator(madWrapper, faultInd1, faultInd2, faultInd3, answers.numChanges)
+    var timeInd = getTimeIndicator(answers.time, ttInd, faultInd1, faultInd2, faultInd3)
+    
+    var indicators = assessment.getIndicatorValues()
+    indicators.put("truthTable", ttInd)
+    indicators.put("fault1", faultInd1)
+    indicators.put("fault2", faultInd2)    
+    indicators.put("fault3", faultInd3)        
+    indicators.put("uselessProbes", answers.numHiddenProbes)
+    indicators.put("usefulProbes", answers.numEvidentProbes)
+    indicators.put("timeTotal", timeInd)
+    
+    inv.put("c_truthValues", answers.c_truthValues) //save correct answer
+    inv.put("s_truthValues", getSubmittedTruthValuesString(g.madWrapper)) //save submitted answer
+    inv.put("c_fault1", answers.c_fault1)
+    inv.put("s_fault1", answers.s_fault1)
+    inv.put("c_fault2", answers.c_fault2)
+    inv.put("s_fault2", answers.s_fault2)    
+    inv.put("c_fault3", answers.c_fault3)
+    inv.put("s_fault3", answers.s_fault3)
+    inv.put("timeTotal", answers.time)
+    inv.put("activityLog", g.activityLog)    
 }
 
 function processUserEvents(madWrapper) {
-	var savedA = null
-	var savedB = null
-	var savedC = null
-	
-	// evident: true if the output of the circuit agrees with the truth table 
-	// of a non-faulty circuit 
-	var evident = false 
-	
-	var a = parseInt(madWrapper.getInitialCIValue("A")) //input switch A
-	var b = parseInt(madWrapper.getInitialCIValue("B")) //input switch B
-	var c = parseInt(madWrapper.getInitialCIValue("C")) //input switch C
-	var y = parseInt(madWrapper.getInitialCIValue("output")) //circuit output (LED)
-	var evident = isEvident(a, b, c, y)
-	
-	glob.activityLog += "Fault 1: Initial switch setting: A=" + a + " B=" + b + " C=" + c + " Y=" + y + "\n"
+    var savedA = null
+    var savedB = null
+    var savedC = null
+    
+    // evident: true if the output of the circuit agrees with the truth table 
+    // of a non-faulty circuit 
+    var evident = false 
+    
+    var a = parseInt(madWrapper.getInitialCIValue("A")) //input switch A
+    var b = parseInt(madWrapper.getInitialCIValue("B")) //input switch B
+    var c = parseInt(madWrapper.getInitialCIValue("C")) //input switch C
+    var y = parseInt(madWrapper.getInitialCIValue("output")) //circuit output (LED)
+    var evident = isEvident(a, b, c, y)
+    
+    g.activityLog += "Fault 1: Initial switch setting: A=" + a + " B=" + b + " C=" + c + " Y=" + y + "\n"
 
-	var fault2Start = -1
-	var fault3Start = -1
-	
-	var steps = madWrapper.getSortedCIVs("step")
-	for (i = 0; i < steps.size(); ++i) {
-		var step = parseInt(steps.get(i).getValue())
-		var t = steps.get(i).getTime()
-		switch (step) {
-			case 1: fault2Start = t; break;
-			case 2: fault3Start = t; break;
-		}
-	}
-	
-	var civs = madWrapper.getSortedCIVs()
-	var wasUserEvent = true
+    var fault2Start = -1
+    var fault3Start = -1
+    
+    var steps = madWrapper.getSortedCIVs("step")
+    for (i = 0; i < steps.size(); ++i) {
+        var step = parseInt(steps.get(i).getValue())
+        var t = steps.get(i).getTime()
+        switch (step) {
+            case 1: fault2Start = t; break;
+            case 2: fault3Start = t; break;
+        }
+    }
+    
+    var civs = madWrapper.getSortedCIVs()
+    var wasUserEvent = true
 
-	var fault = 1 
-		
-	for (var i = 0; i < civs.size(); ++i) {
-		var civ = civs.get(i)
-		var name = civ.getName()
-		var value = civ.getValue()		
-		var timeString = glob.dateFormat.format(new Date(civ.getTime()))
-		var nearFault2Start = Math.abs(civ.getTime() - fault2Start) < 100
-		var nearFault3Start = Math.abs(civ.getTime() - fault3Start) < 100 		
-		var isUserEvent =  (nearFault2Start || nearFault3Start) ? false : true
-		var isSwitchEvent = name == "A" || name == "B" || name == "C"
-		
-		if (name.equals("step")) {
-			fault = parseInt(value) + 1
-		}
-		
-		if (isSwitchEvent) {
-			if (name == "A") { a = value }
-			else if (name == "B") { b = value }
-			else if (name == "C") { c = value }
-		}
-		
-		if (name == "output") {
-			y = parseInt(value);
-		}
-		
-		evident = isEvident(a, b, c, y)
-		
-		if (isUserEvent) {
-			if (wasUserEvent == false) {
-				glob.activityLog += "Fault " + fault + ": initial switch setting: A=" + savedA + " B=" + savedB + " C=" + savedC + " Y=" + y + "\n"				
-			}
-			if (isSwitchEvent) {
-				glob.activityLog += timeString + " - Input " + name + " flipped to " + value + " (A=" + a + " B=" + b + " C=" + c + " Y=" + y + ")\n"
-				++ answers.numChanges
-			}
-			else if (name == "probeLocation") {
-				var ev = evident ? "(evident)" : "(hidden)"
-				glob.activityLog += timeString + " - Probed [" + 
-					viMap.probeMap[parseInt(value)] + "] " + ev + " \n"
-				++ answers.numChanges						
-				if (evident) {
-					++ answers.numEvidentProbes
-				}
-				else {
-					++ answers.numHiddenProbes
-				}					
-			}
-		}
-		else {
-			savedA = a
-			savedB = b
-			savedC = c
-		}			
-		wasUserEvent = isUserEvent
-	}
+    var fault = 1
+
+    for (var i = 0; i < civs.size(); ++i) {
+        var civ = civs.get(i)
+        var name = civ.getName()
+        var value = civ.getValue()
+        var timeString = g.dateFormat.format(new Date(civ.getTime()))
+        var nearFault2Start = Math.abs(civ.getTime() - fault2Start) < 100
+        var nearFault3Start = Math.abs(civ.getTime() - fault3Start) < 100
+        var isUserEvent =  (nearFault2Start || nearFault3Start) ? false : true
+        var isSwitchEvent = name == "A" || name == "B" || name == "C"
+        
+        if (name.equals("step")) {
+            fault = parseInt(value) + 1
+        }
+        
+        if (isSwitchEvent) {
+            if (name == "A") { a = value }
+            else if (name == "B") { b = value }
+            else if (name == "C") { c = value }
+        }
+        
+        if (name == "output") {
+            y = parseInt(value);
+        }
+
+        evident = isEvident(a, b, c, y)
+
+        if (isUserEvent) {
+            if (wasUserEvent == false) {
+                g.activityLog += "Fault " + fault + ": initial switch setting: A=" + savedA + " B=" + savedB + " C=" + savedC + " Y=" + y + "\n"                
+            }
+            if (isSwitchEvent) {
+                g.activityLog += timeString + " - Input " + name + " flipped to " + value + " (A=" + a + " B=" + b + " C=" + c + " Y=" + y + ")\n"
+                ++ answers.numChanges
+            }
+            else if (name == "probeLocation") {
+                var ev = evident ? "(evident)" : "(hidden)"
+                g.activityLog += timeString + " - Probed [" +
+                    g.helper.getProbeSpotLabel(parseInt(value)) + 
+                    "] " + ev + " \n"
+                ++ answers.numChanges
+                if (evident) {
+                    ++ answers.numEvidentProbes
+                }
+                else {
+                    ++ answers.numHiddenProbes
+                }
+            }
+        }
+        else {
+            savedA = a
+            savedB = b
+            savedC = c
+        }
+        wasUserEvent = isUserEvent
+    }
 }
 
 function isEvident(a, b, c, y) {
-	var x = -1
-	
-	if (a == 0) {
-		if (b == 0) {
-			if (c == 0) { x = 1 } //000
-			else { x = 0 } //001
-		}
-		else {
-			if (c == 0) { x = 1 } // 010
-			else { x = 0 } // 011
-		}
-	}
-	else {
-		if (b == 0) {
-			if (c == 0) { x = 0 } // 100
-			else { x = 1 } // 101
-		}
-		else {
-			if (c == 0) { x = 0 } // 110
-			else { x = 0 } // 111
-		}
-	}
-	return x != y
+    var x = -1
+    
+    if (a == 0) {
+        if (b == 0) {
+            if (c == 0) { x = 1 } //000
+            else { x = 0 } //001
+        }
+        else {
+            if (c == 0) { x = 1 } // 010
+            else { x = 0 } // 011
+        }
+    }
+    else {
+        if (b == 0) {
+            if (c == 0) { x = 0 } // 100
+            else { x = 1 } // 101
+        }
+        else {
+            if (c == 0) { x = 0 } // 110
+            else { x = 0 } // 111
+        }
+    }
+    return x != y
 }
 
 function getTimeIndicator(timeTotal, ttInd, faultInd1, faultInd2, faultInd3) {
-	if (ttInd == 0 && faultInd1 == 0 && faultInd2 == 0 && faultInd3 == 0) {
-		return 0
-	}
-	if (timeTotal < 900) {
-		return 2
-	}
-	else if (timeTotal < 1800) {
-		return 1
-	}
-	else {
-		return 0
-	}
+    if (ttInd == 0 && faultInd1 == 0 && faultInd2 == 0 && faultInd3 == 0) {
+        return 0
+    }
+    if (timeTotal < 900) {
+        return 2
+    }
+    else if (timeTotal < 1800) {
+        return 1
+    }
+    else {
+        return 0
+    }
 }
 
 function getNumProbesIndicator(madWrapper, faultInd1, faultInd2, faultInd3, numChanges) {
-	if (faultInd1 == 0 && faultInd2 == 0 && faultInd3 == 0) {
-		return 0
-	}
-	if (numChanges < 25) {
-		return 2
-	}	
-	else if (numChanges < 50) {
-		return 1
-	}
-	else {
-		return 0
-	}
+    if (faultInd1 == 0 && faultInd2 == 0 && faultInd3 == 0) {
+        return 0
+    }
+    if (numChanges < 25) {
+        return 2
+    }    
+    else if (numChanges < 50) {
+        return 1
+    }
+    else {
+        return 0
+    }
 }
 
 function getSubmittedTruthValuesString(madWrapper) {
-	var s = ""
-	
-	for (var i = 0; i < 8; ++i) {
-		var label = "TT" + i
-		s += madWrapper.getLastCIValue(label)
-	}	
-	return s
+    var s = ""
+    
+    for (var i = 0; i < 8; ++i) {
+        var label = "TT" + i
+        s += madWrapper.getLastCIValue(label)
+    }    
+    return s
 }
-
-
