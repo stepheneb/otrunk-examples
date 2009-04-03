4b359467fcee7c5eaaf1562fb9fa855c3716b8fb
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2009-04-03 16:36:47 +0000

    Add example model showing specified agent creation
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@17957 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ EvolutionReadiness/models/scripts/manual-sunlight/setup.js
@@ -0,0 +1,58 @@
+importPackage(Packages.java.lang);
+importPackage(Packages.java.awt);
+importPackage(Packages.org.concord.biologica.environment);
+importPackage(Packages.org.concord.framework.otrunk);
+
+var env
+var world
+  			
+function init() {
+	env = envView.getEnvironment()
+	world = env.getWorld()
+	env.setColumns(20)
+	env.setRows(20)
+	env.setWrapNorthSouth(false)
+	env.setWrapEastWest(false)
+	for (var i = 0; i < 20; i++) {
+	        for (var j = 0; j < 20; j++) {
+	        	var envUnit = new EnvironmentUnit(env, null, i, j)
+	        	env.setEnvironmentUnit(envUnit, i, j)
+	        	envUnit.setProperty("sunlight", new Float(0.8))
+	        	
+	        	var percentSunlight = envUnit.getProperty("sunlight")
+	        	var rg =  (1*percentSunlight.floatValue())
+                var color = new Color(0.7+(rg*0.2), 0.4+(rg*0.2), 0.3)
+	       //		var color = new Color(0.8, 0.5, 0.3)
+	        	var terrain = new Terrain(world, "terrain", color)
+	        	envUnit.setTerrain(terrain)
+            }
+        }
+    sunSlider.addOTChangeListener(sunSliderChangeListener)
+	return true;
+}
+
+var sunSliderChangeHandler =
+{
+    stateChanged: function(evt)
+    {
+        var percentSunlight = (evt.getValue() / 100)
+        var percentSunlightFloat = new Float(percentSunlight)
+        for (var i = 0; i < 20; i++) {
+	        for (var j = 0; j < 20; j++) {
+	        	var envUnit = env.getEnvironmentUnit(i, j)
+	        	envUnit.setProperty("sunlight", percentSunlightFloat)
+	        	
+	        	var color = new Color(0.7+(percentSunlight*0.2), 0.4+(percentSunlight*0.2), 0.3)
+	      // 		var color = new Color(0.8, 0.5, 0.3)
+                var terrain = new Terrain(world, "terrain", color)
+	        	envUnit.setTerrain(terrain)
+            }
+        }
+    }
+
+}
+var sunSliderChangeListener = new OTChangeListener(sunSliderChangeHandler)
+
+function save() {
+	return true;
+}
