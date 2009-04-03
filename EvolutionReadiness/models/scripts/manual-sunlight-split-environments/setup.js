de45be9b495a744f921cb661ba3e2bfb370b5050
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2009-04-03 16:47:51 +0000

    Add model with split environment
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@17958 6e01202a-0783-4428-890a-84243c50cc2b

--- EvolutionReadiness/models/scripts/manual-sunlight-split-environments/setup.js
+++ EvolutionReadiness/models/scripts/manual-sunlight-split-environments/setup.js
@@ -17,7 +17,7 @@ function init() {
 	        for (var j = 0; j < 20; j++) {
 	        	var envUnit = new EnvironmentUnit(env, null, i, j)
 	        	env.setEnvironmentUnit(envUnit, i, j)
-	        	envUnit.setProperty("sunlight", new Float(1.0))
+	        	envUnit.setProperty("sunlight", new Float(0.5))
 	        	
 	        	var percentSunlight = envUnit.getProperty("sunlight")
 	        	var rg =  (1*percentSunlight.floatValue())
@@ -35,14 +35,29 @@ var sunSliderChangeHandler =
 {
     stateChanged: function(evt)
     {
-        var percentSunlight = (evt.getValue() / 100)
-        var percentSunlightFloat = new Float(percentSunlight)
-        for (var i = 0; i < 20; i++) {
+        var difference = (evt.getValue() / 100)
+        var sunlightUpper = 0.5 + (difference / 2)
+        var sunlightLower = 0.5 - (difference / 2)
+        var sunlightUpperFloat = new Float(sunlightUpper)
+        var sunlightLowerFloat = new Float(sunlightLower)
+        
+        for (var i = 0; i < 10; i++) {
 	        for (var j = 0; j < 20; j++) {
 	        	var envUnit = env.getEnvironmentUnit(i, j)
-	        	envUnit.setProperty("sunlight", percentSunlightFloat)
+	        	envUnit.setProperty("sunlight", sunlightUpperFloat)
 	        	
-	        	var color = new Color(0.7+(percentSunlight*0.2), 0.4+(percentSunlight*0.2), 0.3)
+	        	var color = new Color(0.7+(sunlightUpper*0.2), 0.4+(sunlightUpper*0.2), 0.3)
+	      // 		var color = new Color(0.8, 0.5, 0.3)
+                var terrain = new Terrain(world, "terrain", color)
+	        	envUnit.setTerrain(terrain)
+            }
+        }
+        for (var i = 10; i < 20; i++) {
+	        for (var j = 0; j < 20; j++) {
+	        	var envUnit = env.getEnvironmentUnit(i, j)
+	        	envUnit.setProperty("sunlight", sunlightLowerFloat)
+	        	
+	        	var color = new Color(0.7+(sunlightLower*0.2), 0.4+(sunlightLower*0.2), 0.3)
 	      // 		var color = new Color(0.8, 0.5, 0.3)
                 var terrain = new Terrain(world, "terrain", color)
 	        	envUnit.setTerrain(terrain)
