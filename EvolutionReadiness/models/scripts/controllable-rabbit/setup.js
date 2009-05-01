7eb4c8385f491707e2dd47eb2349477f8ba02b47
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2009-05-01 15:43:19 +0000

    Add new controllable-rabbit model and script
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@18532 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ EvolutionReadiness/models/scripts/controllable-rabbit/setup.js
@@ -0,0 +1,124 @@
+importPackage(Packages.java.lang);
+importPackage(Packages.java.awt);
+importPackage(Packages.java.awt.event);
+importPackage(Packages.java.swing);
+importClass(Packages.javax.swing.JOptionPane);
+importPackage(Packages.org.concord.biologica.environment);
+importPackage(Packages.org.concord.framework.otrunk);
+  	
+var rabbit = rabbit_species.createAgent()
+		
+function init() {
+	world = env.getWorld()
+	env.setWrapNorthSouth(false)
+	env.setWrapEastWest(false)
+	for (var i = 0; i < 8; i++) {
+	        for (var j = 0; j < 8; j++) {
+	        	var envUnit = new EnvironmentUnit(env, null, i, j)
+	        	envUnit.setProperty("brownness", new Float(0.0))
+	        	env.setEnvironmentUnit(envUnit, i, j)
+	        	var color = new Color(0.3, 0.8, 0.1)
+	        	
+	        	var terrain = new Terrain(world, "terrain", color)
+	        	envUnit.setTerrain(terrain)
+            }
+        }
+        
+        
+	rabbit.setAge(20)
+//	rabbit.setProperty("Speed", 0)
+	rabbit.setProperty("is immortal", true)
+	rabbit.setProperty("max offspring", 0)
+	env.addAgent(rabbit)
+	
+	for (var i = 0; i < 25; i++) {
+		var plant = plant_species.createAgent();
+		plant.setAge(i*2)
+		env.addAgent(plant)
+	}
+	
+	window.requestFocus()
+	window.addKeyListener(arrowListener)
+	
+	env.addStepListener(stepListener)
+	
+	return true;
+}
+
+var arrowHandler =
+{
+    keyPressed: function(evt)
+    {
+    	var keyCode = evt.getKeyCode()
+    	var loc = rabbit.getLocation()
+    	var newLoc
+    	if (keyCode == evt.VK_UP){
+    		newLoc = new Point(loc.x, loc.y - 3)
+    	} else if (keyCode == evt.VK_DOWN){
+    		newLoc = new Point(loc.x, loc.y + 3)
+    	} else if (keyCode == evt.VK_LEFT){
+    		newLoc = new Point(loc.x - 3, loc.y)
+    	} else if (keyCode == evt.VK_RIGHT){
+    		newLoc = new Point(loc.x + 3, loc.y)
+    	}
+    	rabbit.setLocation(newLoc)
+    }
+
+}
+var arrowListener = new KeyListener(arrowHandler)
+
+var stepHandler =
+{
+	environmentStepped: function()
+	{
+		var hunger = rabbit.getProperty("Hunger").intValue()
+		slider.setValue(hunger * 4)
+		System.out.println("hunger = "+hunger)
+		if (hunger == 250){
+			JOptionPane.showMessageDialog(null, "Oh no! Your rabbit died of hunger.")
+			rabbit.setProperty("Hunger", 0)
+			env.removeAgent(rabbit)
+			JOptionPane.showMessageDialog(null, "Try again!")
+			env.removeAllAgents()
+			rabbit = rabbit_species.createAgent()
+			rabbit.setAge(20)
+			rabbit.setProperty("Speed", 0)
+			rabbit.setProperty("is immortal", true)
+			rabbit.setProperty("max offspring", 0)
+			env.addAgent(rabbit)
+			
+			for (var i = 0; i < 25; i++) {
+				var plant = plant_species.createAgent();
+				plant.setAge(i*2)
+				env.addAgent(plant)
+			}
+		}
+	}
+}
+var stepListener = new EnvironmentStepListener(stepHandler)
+
+var grassSliderChangeHandler =
+{
+    stateChanged: function(evt)
+    {
+        var brownness = ((100 - evt.getValue()) / 100)
+        var brownnessFloat = new Float(brownness)
+        for (var i = 0; i < 20; i++) {
+	        for (var j = 0; j < 20; j++) {
+	        	var envUnit = env.getEnvironmentUnit(i, j)
+	        	envUnit.setProperty("brownness", brownnessFloat)
+	        	
+	        	var color = new Color(0.3+(brownness*0.5), 0.8-(brownness*0.1), 0.1+(brownness*0.3))
+	      // 		var color = new Color(0.8, 0.5, 0.3)
+                var terrain = new Terrain(world, "terrain", color)
+	        	envUnit.setTerrain(terrain)
+            }
+        }
+    }
+
+}
+var grassSliderChangeListener = new OTChangeListener(grassSliderChangeHandler)
+
+function save() {
+	return true;
+}
