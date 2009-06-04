e4d844d2e8aeb42d10529be2b57e20623ca500be
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2009-06-04 16:33:41 +0000

    Add 4th plants activity, update model, update model script to repaint after slider change
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@19036 6e01202a-0783-4428-890a-84243c50cc2b

--- EvolutionReadiness/models/scripts/manual-sunlight/setup.js
+++ EvolutionReadiness/models/scripts/manual-sunlight/setup.js
@@ -21,7 +21,6 @@ function init() {
 	        	var percentSunlight = envUnit.getProperty("sunlight")
 	        	var rg =  (1*percentSunlight.floatValue())
                 var color = new Color(0.7+(rg*0.2), 0.4+(rg*0.2), 0.3)
-	       //		var color = new Color(0.8, 0.5, 0.3)
 	        	var terrain = new Terrain(world, "terrain", color)
 	        	envUnit.setTerrain(terrain)
             }
@@ -47,6 +46,7 @@ var sunSliderChangeHandler =
 	        	envUnit.setTerrain(terrain)
             }
         }
+        holder.repaint()
     }
 
 }
