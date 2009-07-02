d43ee89225db28ebe8204c114c28d6bdf31ce911
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2009-07-02 20:34:58 +0000

    Add background to continuous environment, update script to set sunlight correctly.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@19414 6e01202a-0783-4428-890a-84243c50cc2b

--- EvolutionReadiness/models/scripts/gradated-sunlight/setup.js
+++ EvolutionReadiness/models/scripts/gradated-sunlight/setup.js
@@ -11,17 +11,11 @@ function init() {
 	for (var i = 0; i < 15; i++) {
 	        for (var j = 0; j < 15; j++) {
 	        	var envUnit = new EnvironmentUnit(env, null, i, j)
-	        	env.setEnvironmentUnit(envUnit, i, j)
-	        	envUnit.setProperty("sunlight", new Float(1 - (j/15)))
-	        	
-	        	var percentSunlight = envUnit.getProperty("sunlight")
-	        	var r =  (1*percentSunlight.floatValue()) * 0.5
-	        	var g = r * 0.5
-	        	var b = g * 0.2
-	        	var color = new Color(r+0.3, g+0.2, b+0.1)
-	       //		var color = new Color(0.8, 0.5, 0.3)
-	        	var terrain = new Terrain(world, "terrain", color)
-	        	envUnit.setTerrain(terrain)
+					        	env.setEnvironmentUnit(envUnit, i, j)
+					        	envUnit.setProperty("sunlight", new Float((j+1)/10))
+					        	var color = new Color(0.3, 0.2, 0.1)
+					        	var terrain = new Terrain(world, "terrain", color)
+					        	envUnit.setTerrain(terrain)
             }
         }
 	return true;
